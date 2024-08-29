e3c080285c9f42c937fca5e71f170a2c459f6958
BD103 <59022059+BD103@users.noreply.github.com>
2024-08-29 10:27:50 -0400

    feat: add `bevy_lint_driver`

---
+++ bevy_lint_driver/src/callback.rs
@@ -0,0 +1,19 @@
+use rustc_driver::Callbacks;
+use rustc_interface::interface::Config;
+
+pub struct BevyLintCallback;
+
+impl Callbacks for BevyLintCallback {
+    fn config(&mut self, config: &mut Config) {
+        // We're overwriting `register_lints`, but we don't want to completely delete the original
+        // function. Instead, we save it so we can call it ourselves inside its replacement.
+        let previous = config.register_lints.take();
+
+        config.register_lints = Some(Box::new(move |session, store| {
+            // If there was a previous `register_lints`, call it first.
+            if let Some(previous) = &previous {
+                (previous)(session, store);
+            }
+        }));
+    }
+}
