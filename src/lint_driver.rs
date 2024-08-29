d8e76017c816d7850e046f932654145c067cb9b2
BD103 <59022059+BD103@users.noreply.github.com>
2024-08-29 11:24:34 -0400

    refactor(lint): move driver to `bevy_cli`

---
+++ src/lint_driver.rs
@@ -0,0 +1,10 @@
+//! This is built into the `bevy_lint_driver` executable. This file does not actually contain any
+//! logic, it simply calls [`bevy_lint::driver::main()`] and exits.
+
+// `bevy_lint` uses the `rustc_private` feature, so in order for us to call `bevy_lint` we must also
+// opt-in to `rustc_private`.
+#![feature(rustc_private)]
+
+fn main() {
+    bevy_lint::driver::main()
+}
