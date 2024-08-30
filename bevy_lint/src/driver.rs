46d0555fbf91dbbbac12811adf9767dcb3e796a3
BD103 <59022059+BD103@users.noreply.github.com>
2024-08-30 10:52:28 -0400

    Return `ExitCode` from `driver::main()` (#23)
    Closes #17!

--- bevy_lint/src/driver.rs
+++ bevy_lint/src/driver.rs
@@ -1,14 +1,17 @@
 //! Contains code related to writing `bevy_lint_driver`.
 
 use crate::callback::BevyLintCallback;
+use std::process::ExitCode;
 
 /// This is the main entrypoint into the driver, exported so that `bevy_cli` may call it.
-#[allow(clippy::result_unit_err)]
-pub fn main() -> Result<(), ()> {
+pub fn main() -> ExitCode {
     let args: Vec<String> = dbg!(std::env::args().skip(1).collect());
 
     // Call the compiler with our custom callback.
-    rustc_driver::RunCompiler::new(&args, &mut BevyLintCallback)
-        .run()
-        .map_err(|_| ())
+    let result = rustc_driver::RunCompiler::new(&args, &mut BevyLintCallback).run();
+
+    match result {
+        Ok(_) => ExitCode::SUCCESS,
+        Err(_) => ExitCode::FAILURE,
+    }
 }
