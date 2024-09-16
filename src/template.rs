d96fde222782ed03f60a540a371aa0c48e0a48c3
Rich Churcher <rich.churcher@gmail.com>
2024-09-16 16:49:42 +1200

    bare-bones -> minimal

--- src/template.rs
+++ src/template.rs
@@ -20,7 +20,7 @@ pub fn generate_template(name: &str, git: Option<&str>) -> anyhow::Result<PathBu
 ///
 /// If `git` is [`None`], it will default to `bevy_quickstart`.
 fn template_path(git: Option<&str>) -> TemplatePath {
-    // Use a bare-bones template by default
+    // Use a minimal template by default.
     const DEFAULT_REPOSITORY: &str = "https://github.com/TheBevyFlock/bevy_new.git";
     const DEFAULT_BRANCH: &str = "main";
 
