8166785700b584e8059cb9d22a2a8922ad8e5bff
BD103 <59022059+BD103@users.noreply.github.com>
2024-09-16 20:13:16 -0400

    refactor: replace `define_lints!` macro with hardcoded values
    `rustfmt` doesn't format the lint modules because it can't find `mod name;` syntax for them. And honestly? This is fine until it becomes incredibly cumbersome.

--- bevy_lint/src/lints/mod.rs
+++ bevy_lint/src/lints/mod.rs
@@ -1,22 +1,5 @@
 use rustc_lint::Lint;
 
-macro_rules! define_lints {
-    {
-        $(mod $module:ident {
-            lint: $lint:ident$(,)?
-        })*
-    } => {
-        // Declare all modules as private.
-        $(pub mod $module;)*
+pub mod main_return_without_appexit;
 
-        pub static LINTS: &[&Lint] = &[
-            $(self::$module::$lint,)*
-        ];
-    };
-}
-
-define_lints! {
-    mod main_return_without_appexit {
-        lint: MAIN_RETURN_WITHOUT_APPEXIT,
-    }
-}
+pub static LINTS: &[&Lint] = &[main_return_without_appexit::MAIN_RETURN_WITHOUT_APPEXIT];
