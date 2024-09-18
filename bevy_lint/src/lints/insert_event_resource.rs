37d33c0931be9dd0ea4132fea6fa0ffb4f400169
BD103 <59022059+BD103@users.noreply.github.com>
2024-09-17 22:39:52 -0400

    feat: add suggestions to diagnostic for `init_resource()`

--- bevy_lint/src/lints/insert_event_resource.rs
+++ bevy_lint/src/lints/insert_event_resource.rs
@@ -1,11 +1,18 @@
 //! TODO
 
-use clippy_utils::{diagnostics::span_lint, peel_middle_ty_refs, sym, ty::match_type};
-use rustc_hir::{Expr, ExprKind, GenericArg, GenericArgs, PathSegment};
+use clippy_utils::{
+    diagnostics::{span_lint, span_lint_and_sugg},
+    source::snippet_with_applicability,
+    sym,
+    ty::match_type,
+};
+use rustc_errors::Applicability;
+use rustc_hir::{Expr, ExprKind, GenericArg, GenericArgs, Path, PathSegment, QPath, Ty, TyKind};
 use rustc_hir_analysis::lower_ty;
 use rustc_lint::{LateContext, LateLintPass};
 use rustc_session::{declare_lint_pass, declare_tool_lint};
 use rustc_span::Span;
+use std::borrow::Cow;
 
 declare_tool_lint! {
     pub bevy::INSERT_EVENT_RESOURCE,
@@ -68,7 +75,7 @@ fn check_insert_resource<'tcx>(cx: &LateContext<'tcx>, args: &[Expr], method_spa
 fn check_init_resource<'tcx>(cx: &LateContext<'tcx>, path: &PathSegment<'tcx>, method_span: Span) {
     if let Some(&GenericArgs {
         // `App::init_resource()` has one generic type argument: T.
-        args: &[GenericArg::Type(generic_ty)],
+        args: &[GenericArg::Type(generic_hir_ty)],
         ..
     }) = path.args
     {
@@ -76,15 +83,57 @@ fn check_init_resource<'tcx>(cx: &LateContext<'tcx>, path: &PathSegment<'tcx>, m
         // information, see <https://rustc-dev-guide.rust-lang.org/ty.html#rustc_hirty-vs-tyty>.
         // Note that `lower_ty()` is quasi-deprecated, and should be removed if a adequate
         // replacement is found.
-        let generic_ty = lower_ty(cx.tcx, generic_ty);
+        let generic_ty = lower_ty(cx.tcx, generic_hir_ty);
 
         // If the generic argument is `Events<T>`, emit the lint.
         if match_type(cx, generic_ty, &crate::paths::EVENTS) {
-            span_lint(
+            let mut applicability = Applicability::MachineApplicable;
+
+            // Get the `str` representation of the type; this will usually look like
+            // `Events<MyEvent>`. If the snippet cannot be found, fallback to the default and
+            // update the applicability to reflect this.
+            let generic_ty_span = match generic_hir_ty.kind {
+                TyKind::Path(QPath::Resolved(
+                    _,
+                    &Path {
+                        segments:
+                            [PathSegment {
+                                args:
+                                    Some(GenericArgs {
+                                        args: [GenericArg::Type(Ty { span, .. })],
+                                        ..
+                                    }),
+                                ..
+                            }],
+                        ..
+                    },
+                )) => Some(*span),
+                _ => None,
+            };
+
+            const DEFAULT_SNIPPET: &str = "Events<T>";
+
+            let ty_snippet = match generic_ty_span {
+                Some(generic_ty_span) => snippet_with_applicability(
+                    cx,
+                    generic_ty_span,
+                    DEFAULT_SNIPPET,
+                    &mut applicability,
+                ),
+                None => {
+                    applicability = Applicability::HasPlaceholders;
+                    Cow::Borrowed(DEFAULT_SNIPPET)
+                }
+            };
+
+            span_lint_and_sugg(
                 cx,
                 INSERT_EVENT_RESOURCE,
                 method_span,
-                INSERT_EVENT_RESOURCE.desc,
+                "called `App::init_resource::<Events<T>>()` instead of `App::add_event::<T>()`", /* TODO: customize */
+                "use",
+                format!("add_event::<{ty_snippet}>()"),
+                applicability,
             );
         }
     }
