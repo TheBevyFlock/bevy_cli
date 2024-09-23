cd46cc6b1886d046086fa0d7f33b70ecaf38878b
BD103 <59022059+BD103@users.noreply.github.com>
2024-09-23 14:56:13 -0400

    feat!: define group within lint definitions

---
+++ bevy_lint/src/lint.rs
@@ -0,0 +1,47 @@
+use rustc_lint::{Level, Lint};
+
+/// A Bevy lint definition and its associated group.
+///
+/// The level of the lint must be the same as the level of the group.
+pub struct BevyLint {
+    pub lint: &'static Lint,
+    pub group: &'static LintGroup,
+}
+
+/// Represents a lint group.
+pub struct LintGroup {
+    /// The name of the lint group.
+    ///
+    /// This will be used when trying to enable / disable the group, such as through
+    /// `#![allow(group)]`. By convention, this should start with `bevy::`.
+    pub name: &'static str,
+
+    // The default level all lints within this group should be.
+    pub level: Level,
+}
+
+#[macro_export]
+macro_rules! declare_bevy_lint {
+    {
+        $(#[$attr:meta])*
+        $vis:vis $name:ident,
+        $group:ident,
+        $desc:expr
+    } => {
+        $(#[$attr])*
+        $vis static $name: &$crate::lint::BevyLint = &$crate::lint::BevyLint {
+            lint: &::rustc_lint::Lint {
+                name: concat!("bevy::", stringify!($name)),
+                default_level: $crate::groups::$group.level,
+                desc: $desc,
+                edition_lint_opts: None,
+                report_in_external_macro: false,
+                future_incompatible: None,
+                is_externally_loaded: true,
+                feature_gate: None,
+                crate_level_only: false,
+            },
+            group: &$crate::groups::$group,
+        };
+    };
+}
--- bevy_lint/src/lints/mod.rs
+++ bevy_lint/src/lints/mod.rs
@@ -1,71 +1,17 @@
-use rustc_lint::{Lint, LintId, LintStore};
+use crate::BevyLint;
+use rustc_lint::{Lint, LintStore};
 
 pub mod insert_event_resource;
 pub mod main_return_without_appexit;
 
-/// A group of deny-by-default lints that check for outright wrong or useless code.
-///
-/// These lints are carefully picked to be free of false-positives. You should avoid
-/// `#[allow(...)]`-ing these lints without a _very_ good reason.
-pub static CORRECTNESS: &[&Lint] = &[];
-
-/// A group similar to [`CORRECTNESS`] that checks for suspicious or usually wrong code.
-///
-/// As compared to [`CORRECTNESS`], it may be possible that the linted code may be written
-/// intentionally. Even still, you usually want to fix these lints instead of `#[allow(...)]`-ing
-/// them.
-pub static SUSPICIOUS: &[&Lint] = &[insert_event_resource::INSERT_EVENT_RESOURCE];
-
-/// A group that offers suggestions on how to simplify your code.
-pub static COMPLEXITY: &[&Lint] = &[];
-
-/// A group that suggests how to increase the performance of your code.
-pub static PERFORMANCE: &[&Lint] = &[];
-
-/// A group of lints that encourage idiomatic code.
-///
-/// These lints are opinionated and may be freely disabled if you disagree with their suggestions.
-pub static STYLE: &[&Lint] = &[];
-
-/// A group of lints that make the linter incredibly nit-picky.
-///
-/// If you enable this group, expect to liberally apply `#[allow(...)]` attributes throughout your
-/// code.
-pub static PEDANTIC: &[&Lint] = &[main_return_without_appexit::MAIN_RETURN_WITHOUT_APPEXIT];
-
-/// A group of opt-in lints that restrict you from writing certain code.
-///
-/// These are designed for scenarios where you want to increase the consistency of your code-base
-/// and reject certain patterns. They should not all be enabled at once, but instead specific lints
-/// should be individually enabled.
-pub static RESTRICTION: &[&Lint] = &[];
+pub static LINTS: &[&BevyLint] = &[
+    insert_event_resource::INSERT_EVENT_RESOURCE,
+    main_return_without_appexit::MAIN_RETURN_WITHOUT_APPEXIT,
+];
 
 pub(crate) fn register_lints(store: &mut LintStore) {
-    store.register_lints(CORRECTNESS);
-    store.register_lints(SUSPICIOUS);
-    store.register_lints(COMPLEXITY);
-    store.register_lints(PERFORMANCE);
-    store.register_lints(STYLE);
-    store.register_lints(PEDANTIC);
-    store.register_lints(RESTRICTION);
-
-    /// Shorthand for registering a group of lints.
-    fn register_group(store: &mut LintStore, name: &'static str, lints: &'static [&'static Lint]) {
-        store.register_group(
-            true,
-            name,
-            None,
-            lints.iter().copied().map(LintId::of).collect(),
-        );
-    }
-
-    register_group(store, "bevy::correctness", CORRECTNESS);
-    register_group(store, "bevy::suspicious", SUSPICIOUS);
-    register_group(store, "bevy::complexity", COMPLEXITY);
-    register_group(store, "bevy::performance", PERFORMANCE);
-    register_group(store, "bevy::style", STYLE);
-    register_group(store, "bevy::pedantic", PEDANTIC);
-    register_group(store, "bevy::restriction", RESTRICTION);
+    let lints: Vec<&Lint> = LINTS.iter().map(|x| x.lint).collect();
+    store.register_lints(&lints);
 }
 
 pub(crate) fn register_passes(store: &mut LintStore) {
