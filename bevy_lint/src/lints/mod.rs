09d994f1dd869e74e86a99c4c692e7eb6258e98f
BD103 <59022059+BD103@users.noreply.github.com>
2024-09-27 14:51:25 -0400

    Add lint: `panicking_query_methods` and `panicking_world_methods` (#95)
    As per @alice-i-cecile's request! Closes #58.
    
    This adds the `bevy::panicking_query_methods` lint, which searches for
    usage of panicking methods like `Query::single()`. It is currently
    `Allow`-by-default, so you need to explicitly opt-in to use it. (In the
    [project](https://github.com/orgs/TheBevyFlock/projects/3/views/1?sliceBy%5Bvalue%5D=Restriction)
    it's under the Restriction category.)
    
    This lint checks both `Query` and `QueryState`. I will write a lint for
    resources in another PR. :)
    
    ## Testing
    
    Wow, I'm so glad you asked! (You asked how to test this lint, right?)
    
    I used the following example when testing. Paste it into
    `bevy_lint/examples/lint_test.rs`, run `cd bevy_lint`, then run `cargo
    build && cargo run -- --example lint_test`.
    
    ```rust
    #![feature(register_tool)]
    #![register_tool(bevy)]
    
    #![deny(bevy::panicking_query_methods)]
    #![deny(bevy::panicking_world_methods)]
    
    use bevy::prelude::*;
    
    #[derive(Component)]
    struct MyComponent;
    
    #[derive(Resource)]
    struct MyResource;
    
    fn main() {
        App::new().add_systems(Update, my_system);
    
        let mut world = World::new();
    
        let mut query_state = QueryState::<&mut MyComponent>::new(&mut world);
        let _ = query_state.single_mut(&mut world);
    
        world.insert_resource(MyResource);
        let _ = world.resource::<MyResource>();
    }
    
    fn my_system(query: Query<&MyComponent>) {
        let _ = query.many([]);
    }
    ```
    
    The warnings should look like this:
    
    ```bash
    error: called a `QueryState` method that can panic when a non-panicking alternative exists
      --> bevy_lint/examples/lint_test.rs:21:25
       |
    21 |     let _ = query_state.single_mut(&mut world);
       |                         ^^^^^^^^^^^^^^^^^^^^^^
       |
       = help: use `query_state.get_single_mut(&mut world)` and handle the `Option` or `Result`
    note: the lint level is defined here
      --> bevy_lint/examples/lint_test.rs:4:9
       |
    4  | #![deny(bevy::panicking_query_methods)]
       |         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    
    error: called a `World` method that can panic when a non-panicking alternative exists
      --> bevy_lint/examples/lint_test.rs:24:19
       |
    24 |     let _ = world.resource::<MyResource>();
       |                   ^^^^^^^^^^^^^^^^^^^^^^^^
       |
       = help: use `world.get_resource::<MyResource>()` and handle the `Option` or `Result`
    note: the lint level is defined here
      --> bevy_lint/examples/lint_test.rs:5:9
       |
    5  | #![deny(bevy::panicking_world_methods)]
       |         ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
    
    error: called a `Query` method that can panic when a non-panicking alternative exists
      --> bevy_lint/examples/lint_test.rs:28:19
       |
    28 |     let _ = query.many([]);
       |                   ^^^^^^^^
       |
       = help: use `query.get_many([])` and handle the `Option` or `Result`
    ```
    
    Fiddle around with it and try to break it! Good luck, though. Unless you
    use #94, this is the most foolproof lint I've written so far! >:D

--- bevy_lint/src/lints/mod.rs
+++ bevy_lint/src/lints/mod.rs
@@ -3,10 +3,13 @@ use rustc_lint::{Lint, LintStore};
 
 pub mod insert_event_resource;
 pub mod main_return_without_appexit;
+pub mod panicking_methods;
 
 pub(crate) static LINTS: &[&BevyLint] = &[
     insert_event_resource::INSERT_EVENT_RESOURCE,
     main_return_without_appexit::MAIN_RETURN_WITHOUT_APPEXIT,
+    panicking_methods::PANICKING_QUERY_METHODS,
+    panicking_methods::PANICKING_WORLD_METHODS,
 ];
 
 pub(crate) fn register_lints(store: &mut LintStore) {
@@ -17,4 +20,5 @@ pub(crate) fn register_lints(store: &mut LintStore) {
 pub(crate) fn register_passes(store: &mut LintStore) {
     store.register_late_pass(|_| Box::new(insert_event_resource::InsertEventResource));
     store.register_late_pass(|_| Box::new(main_return_without_appexit::MainReturnWithoutAppExit));
+    store.register_late_pass(|_| Box::new(panicking_methods::PanickingMethods));
 }
