7543333179624c951a2092ab17049a2fbdccfff3
Tim Jentzsch <commits@timjen.net>
2024-09-29 11:59:43 +0200

    Add functionality to run the bevy app

---
+++ src/run/args.rs
@@ -0,0 +1,48 @@
+use clap::{ArgAction, Args, Subcommand};
+
+use crate::external_cli::{arg_builder::ArgBuilder, cargo::run::CargoRunArgs};
+
+#[derive(Debug, Args)]
+pub struct RunArgs {
+    /// The subcommands available for the run command.
+    #[command(subcommand)]
+    pub subcommand: Option<RunSubcommands>,
+
+    /// Commands to forward to `cargo run`.
+    #[clap(flatten)]
+    pub cargo_args: CargoRunArgs,
+}
+
+impl RunArgs {
+    /// Whether to run the app in the browser.
+    pub(crate) fn is_web(&self) -> bool {
+        matches!(self.subcommand, Some(RunSubcommands::Web(_)))
+    }
+
+    /// The profile used to compile the app.
+    pub(crate) fn profile(&self) -> &str {
+        self.cargo_args.compilation_args.profile()
+    }
+
+    /// Generate arguments for `cargo`.
+    pub(crate) fn cargo_args_builder(&self) -> ArgBuilder {
+        self.cargo_args.args_builder(self.is_web())
+    }
+}
+
+#[derive(Debug, Subcommand)]
+pub enum RunSubcommands {
+    /// Run your app in the browser.
+    Web(RunWebArgs),
+}
+
+#[derive(Debug, Args)]
+pub struct RunWebArgs {
+    /// The port to run the web server on.
+    #[arg(short, long, default_value_t = 4000)]
+    pub port: u16,
+
+    /// Open the app in the browser.
+    #[arg(short = 'o', long = "open", action = ArgAction::SetTrue, default_value_t = false)]
+    pub do_open: bool,
+}
---
+++ src/run/mod.rs
@@ -0,0 +1,48 @@
+use args::RunSubcommands;
+
+use crate::{
+    build::ensure_web_setup,
+    external_cli::{cargo, wasm_bindgen, CommandHelpers},
+    manifest::package_name,
+};
+
+pub use self::args::RunArgs;
+
+mod args;
+mod serve;
+
+pub fn run(args: &RunArgs) -> anyhow::Result<()> {
+    let cargo_args = args.cargo_args_builder();
+
+    if let Some(RunSubcommands::Web(web_args)) = &args.subcommand {
+        ensure_web_setup()?;
+
+        // If targeting the web, run a web server with the WASM build
+        println!("Building for WASM...");
+        cargo::build::command().args(cargo_args).ensure_status()?;
+
+        println!("Bundling for the web...");
+        wasm_bindgen::bundle(&package_name()?, args.profile())?;
+
+        let port = web_args.port;
+        let url = format!("http://127.0.0.1:{port}");
+
+        // Serving the app is blocking, so we open the page first
+        if web_args.do_open {
+            if webbrowser::open(&url).is_err() {
+                println!("Failed to open the browser automatically, open the app on <{url}>");
+            } else {
+                println!("Your app is running on <{url}>");
+            }
+        } else {
+            println!("Open your app on <{url}>");
+        }
+
+        serve::serve(port, args.profile())?;
+    } else {
+        // For native builds, wrap `cargo run`
+        cargo::run::command().args(cargo_args).ensure_status()?;
+    }
+
+    Ok(())
+}
