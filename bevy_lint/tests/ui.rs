bb29bf882a2d43752fb291980156ffa5799bac34
BD103 <59022059+BD103@users.noreply.github.com>
2024-10-02 09:30:43 -0400

    feat: further progress on entrypoint

--- bevy_lint/tests/ui.rs
+++ bevy_lint/tests/ui.rs
@@ -1,20 +1,60 @@
-use ui_test::{run_tests, CommandBuilder, Config};
+use std::{ffi::OsString, path::Path};
 
-fn main() -> ui_test::color_eyre::Result<()> {
+use ui_test::{
+    color_eyre, default_any_file_filter, run_tests_generic, status_emitter, Args, CommandBuilder,
+    Config,
+};
+
+fn main() -> color_eyre::Result<()> {
     let config = config();
-    run_tests(config)
+    run_lint_tests(config)
 }
 
 fn config() -> Config {
     Config {
-        host: None,
+        host: Some(String::new()),
         root_dir: "tests/ui".into(),
-        program: program(),
+        // program: program(),
         out_dir: "../target/ui".into(),
-        ..todo!()
+        ..Config::dummy()
     }
 }
 
 fn program() -> CommandBuilder {
     todo!()
+}
+
+fn run_lint_tests(mut config: Config) -> color_eyre::Result<()> {
+    // Parse arguments from the CLI.
+    let args = Args::test()?;
+
+    config.with_args(&args);
+
+    run_tests_generic(
+        vec![config],
+        // Do not match any files.
+        |path, config| {
+            // We can only run Rust files within the `src/bin` folder.
+            if path.extension()? == "rs" && path.parent()? == Path::new("tests/ui/src/bin") {
+                return Some(default_any_file_filter(path, config));
+            }
+
+            None
+        },
+        // Append `--bin file_stem` to the `cargo check` command.
+        |config, spanned| {
+            let mut argument = OsString::from("--bin ");
+
+            let file_stem = spanned
+                .span
+                .file
+                .file_stem() // Like `file_name()`, but it ignores the extension.
+                .expect("Attempted to test a file without a name.");
+
+            argument.push(file_stem);
+
+            config.program.args.push(argument);
+        },
+        status_emitter::Text::verbose(),
+    )
 }
