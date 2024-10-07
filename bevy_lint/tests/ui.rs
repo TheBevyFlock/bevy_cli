d6058f0ab6731ea64e4fa89547d5aedeeb702a92
BD103 <59022059+BD103@users.noreply.github.com>
2024-10-07 11:21:05 -0400

    feat: detect path to `libbevy.rlib`

--- bevy_lint/tests/ui.rs
+++ bevy_lint/tests/ui.rs
@@ -1,4 +1,12 @@
-use std::path::{Path, PathBuf};
+// A convenience feature used in `find_bevy_rlib()`.
+#![feature(let_chains)]
+
+use serde::Deserialize;
+use std::{
+    ffi::OsStr,
+    path::{Path, PathBuf},
+    process::{Command, Stdio},
+};
 use ui_test::{
     color_eyre::{self, eyre::ensure},
     run_tests, CommandBuilder, Config,
@@ -17,6 +25,14 @@ fn config() -> color_eyre::Result<Config> {
     const DRIVER_PATH: &str = "../target/debug/bevy_lint_driver";
 
     ensure!(Path::new(DRIVER_PATH).is_file());
+
+    let bevy_extern_argument = match find_bevy_rlib() {
+        Ok(path) => format!("--extern=bevy={}", path.display()).into(),
+        Err(error) => {
+            eprintln!("Error while finding path to `libbevy.rlib`: {:?}", error);
+            "--extern=bevy".into()
+        }
+    };
 
     let config = Config {
         // When `host` is `None`, `ui_test` will attempt to auto-discover the host by calling
@@ -41,7 +57,7 @@ fn config() -> color_eyre::Result<Config> {
                 "-L".into(),
                 "all=../target/debug/deps".into(),
                 // This lets UI tests write `use bevy::*;` without `extern bevy;` first.
-                "--extern=bevy".into(),
+                bevy_extern_argument,
             ],
 
             out_dir_flag: Some("--out-dir".into()),
@@ -54,4 +70,56 @@ fn config() -> color_eyre::Result<Config> {
     };
 
     Ok(config)
+}
+
+#[derive(Deserialize, Debug)]
+#[serde(rename = "compiler-artifact", tag = "reason")]
+struct ArtifactMessage<'a> {
+    package_id: &'a str,
+    target: ArtifactTarget<'a>,
+    filenames: Vec<&'a Path>,
+}
+
+#[derive(Deserialize, Debug)]
+struct ArtifactTarget<'a> {
+    name: &'a str,
+    kind: Vec<&'a str>,
+}
+
+fn find_bevy_rlib() -> color_eyre::Result<PathBuf> {
+    let output = Command::new("cargo")
+        .arg("build")
+        .arg("--test=ui")
+        .arg("--message-format=json")
+        .stderr(Stdio::inherit())
+        .output()?;
+
+    ensure!(output.status.success());
+
+    const NEWLINE: u8 = '\n' as u8;
+    const BEVY_PACKAGE_ID_PREFIX: &str =
+        "registry+https://github.com/rust-lang/crates.io-index#bevy@";
+
+    let mut messages = Vec::with_capacity(1);
+
+    for line in output.stdout.split(|&byte| byte == NEWLINE) {
+        if let Ok(message) = serde_json::from_slice::<ArtifactMessage>(line)
+            && message.package_id.starts_with(BEVY_PACKAGE_ID_PREFIX)
+            && message.target.name == "bevy"
+            && message.target.kind == ["lib"]
+        {
+            messages.push(message);
+        }
+    }
+
+    ensure!(messages.len() == 1);
+
+    let rlib = messages[0]
+        .filenames
+        .iter()
+        .filter(|p| p.extension() == Some(OsStr::new("rlib")))
+        .next()
+        .unwrap();
+
+    Ok(rlib.to_path_buf())
 }
