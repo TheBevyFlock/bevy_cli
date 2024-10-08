aab42a435b0aef33daf8ccfff5def3bec00649cf
BD103 <59022059+BD103@users.noreply.github.com>
2024-10-07 21:20:46 -0400

    feat: add ui tests for `main_return_without_appexit`

--- bevy_lint/tests/ui/main_return_without_appexit/base.rs
+++ bevy_lint/tests/ui/main_return_without_appexit/base.rs
@@ -1,3 +1,5 @@
+//! Tests the most basic version: where `main()` returns nothing and `AppExit` is not handled.
+
 #![feature(register_tool)]
 #![register_tool(bevy)]
 #![deny(bevy::main_return_without_appexit)]
@@ -5,6 +7,7 @@
 use bevy::prelude::*;
 
 fn main() {
+    //~^ HELP: try
     App::new().run();
     //~^ ERROR: an entrypoint that calls `App::run()` does not return `AppExit`
 }
---
+++ bevy_lint/tests/ui/main_return_without_appexit/return_result.rs
@@ -0,0 +1,16 @@
+//! Tests when `main()` returns a type other than the unit `()`. When this is done no lint is
+//! emitted, since we assume the user knows what they're doing.
+
+//@check-pass
+
+#![feature(register_tool)]
+#![register_tool(bevy)]
+#![deny(bevy::main_return_without_appexit)]
+
+use bevy::prelude::*;
+
+fn main() -> Result<(), ()> {
+    App::new().run();
+
+    Ok(())
+}
