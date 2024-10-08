cad6bc6fc6895581df23dd615818b1bfd3d6d804
BD103 <59022059+BD103@users.noreply.github.com>
2024-10-07 21:20:46 -0400

    feat: add ui tests for `main_return_without_appexit`

---
+++ bevy_lint/tests/ui/main_return_without_appexit/bug_87.rs
@@ -0,0 +1,17 @@
+//! This test tracks the bug reported in [#87]. When this starts failing, the bug has been fixed.
+//!
+//! [#87]: https://github.com/TheBevyFlock/bevy_cli/issues/87
+
+#![feature(register_tool)]
+#![register_tool(bevy)]
+#![deny(bevy::main_return_without_appexit)]
+
+use bevy::prelude::*;
+
+fn main() {
+    // This should not raise an error, since `AppExit` is not ignored.
+    let app_exit = App::new().run();
+    //~^ ERROR: an entrypoint that calls `App::run()` does not return `AppExit`
+
+    println!("{app_exit:?}");
+}
