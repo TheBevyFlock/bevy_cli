71e35886b0710185bd40dd2534759051e3577226
BD103 <59022059+BD103@users.noreply.github.com>
2024-10-08 22:12:17 -0400

    feat: add help messages to panicking world methods test

--- bevy_lint/tests/ui/panicking_methods/world.rs
+++ bevy_lint/tests/ui/panicking_methods/world.rs
@@ -22,31 +22,41 @@ fn main() {
 
     world.entity(bob);
     //~^ ERROR: called a `World` method that can panic when a non-panicking alternative exists
+    //~| HELP: use `world.get_entity(bob)`
 
     world.entity_mut(bob);
     //~^ ERROR: called a `World` method that can panic when a non-panicking alternative exists
+    //~| HELP: use `world.get_entity_mut(bob)`
 
     world.many_entities([bob]);
     //~^ ERROR: called a `World` method that can panic when a non-panicking alternative exists
+    //~| HELP: use `world.get_many_entities([bob])`
 
     world.many_entities_mut([bob]);
     //~^ ERROR: called a `World` method that can panic when a non-panicking alternative exists
+    //~| HELP: use `world.get_many_entities_mut([bob])`
 
     world.resource::<Jeffrey>();
     //~^ ERROR: called a `World` method that can panic when a non-panicking alternative exists
+    //~| HELP: use `world.get_resource::<Jeffrey>()`
 
     world.resource_mut::<Jeffrey>();
     //~^ ERROR: called a `World` method that can panic when a non-panicking alternative exists
+    //~| HELP: use `world.get_resource_mut::<Jeffrey>()`
 
     world.resource_ref::<Jeffrey>();
     //~^ ERROR: called a `World` method that can panic when a non-panicking alternative exists
+    //~| HELP: use `world.get_resource_ref::<Jeffrey>()`
 
     world.non_send_resource::<Patrick>();
     //~^ ERROR: called a `World` method that can panic when a non-panicking alternative exists
+    //~| HELP: use `world.get_non_send_resource::<Patrick>()`
 
     world.non_send_resource_mut::<Patrick>();
     //~^ ERROR: called a `World` method that can panic when a non-panicking alternative exists
+    //~| HELP: use `world.get_non_send_resource_mut::<Patrick>()`
 
     world.schedule_scope(Update, |_world, _schedule| {});
     //~^ ERROR: called a `World` method that can panic when a non-panicking alternative exists
+    //~| HELP: use `world.try_schedule_scope(Update, |_world, _schedule| {})`
 }
