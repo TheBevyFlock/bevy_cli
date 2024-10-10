78f17704cdafdcd7360f5d387b4b2ecc1a1d6cbb
BD103 <59022059+BD103@users.noreply.github.com>
2024-10-09 22:14:09 -0400

    feat: setup scaffold

---
+++ bevy_lint/src/lints/missing_reflect.rs
@@ -0,0 +1,18 @@
+//! TODO
+
+use rustc_lint::LateLintPass;
+use rustc_session::declare_lint_pass;
+
+use crate::declare_bevy_lint;
+
+declare_bevy_lint! {
+    pub MISSING_REFLECT,
+    RESTRICTION,
+    "defined a component, resource, or event without a `Reflect` implementation",
+}
+
+declare_lint_pass! {
+    MissingReflect => [MISSING_REFLECT.lint]
+}
+
+impl<'tcx> LateLintPass<'tcx> for MissingReflect {}
--- bevy_lint/src/lints/mod.rs
+++ bevy_lint/src/lints/mod.rs
@@ -3,12 +3,14 @@ use rustc_lint::{Lint, LintStore};
 
 pub mod insert_event_resource;
 pub mod main_return_without_appexit;
+pub mod missing_reflect;
 pub mod panicking_methods;
 pub mod plugin_not_ending_in_plugin;
 
 pub(crate) static LINTS: &[&BevyLint] = &[
     insert_event_resource::INSERT_EVENT_RESOURCE,
     main_return_without_appexit::MAIN_RETURN_WITHOUT_APPEXIT,
+    missing_reflect::MISSING_REFLECT,
     panicking_methods::PANICKING_QUERY_METHODS,
     panicking_methods::PANICKING_WORLD_METHODS,
     plugin_not_ending_in_plugin::PLUGIN_NOT_ENDING_IN_PLUGIN,
@@ -22,6 +24,7 @@ pub(crate) fn register_lints(store: &mut LintStore) {
 pub(crate) fn register_passes(store: &mut LintStore) {
     store.register_late_pass(|_| Box::new(insert_event_resource::InsertEventResource));
     store.register_late_pass(|_| Box::new(main_return_without_appexit::MainReturnWithoutAppExit));
+    store.register_late_pass(|_| Box::new(missing_reflect::MissingReflect));
     store.register_late_pass(|_| Box::new(panicking_methods::PanickingMethods));
     store.register_late_pass(|_| Box::new(plugin_not_ending_in_plugin::PluginNotEndingInPlugin));
 }
