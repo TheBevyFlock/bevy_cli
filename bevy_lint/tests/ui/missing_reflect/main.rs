1b672902499003ffbbb41ac7648e4afd8a30daea
BD103 <59022059+BD103@users.noreply.github.com>
2024-10-11 19:52:13 -0400

    feat: create initial `missing_reflect` ui tests

---
+++ bevy_lint/tests/ui/missing_reflect/main.rs
@@ -0,0 +1,32 @@
+#![feature(register_tool)]
+#![register_tool(bevy)]
+#![allow(dead_code)]
+#![deny(bevy::missing_reflect)]
+
+use bevy::{ecs::component::StorageType, prelude::*};
+
+#[derive(Component)]
+struct Missing;
+//~^ ERROR: defined a component, resource, or event without a `Reflect` implementation
+
+struct MissingImpl;
+
+// TODO: This behavior is strange. The lint should be emitted on `struct MissingImpl`.
+//~v ERROR: defined a component, resource, or event without a `Reflect` implementation
+impl Component for MissingImpl {
+    const STORAGE_TYPE: StorageType = StorageType::Table;
+}
+
+#[derive(Component, Reflect)]
+struct Satisfied;
+
+#[allow(bevy::missing_reflect)]
+#[derive(Component)]
+struct Muted;
+
+#[allow(bevy::missing_reflect)]
+struct MutedImpl;
+
+impl Component for MutedImpl {
+    const STORAGE_TYPE: StorageType = StorageType::Table;
+}
