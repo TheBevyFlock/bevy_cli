f49ea78a835b0b795ed68b1656424882f1b6e331
BD103 <59022059+BD103@users.noreply.github.com>
2024-10-15 08:54:37 -0400

    feat: redo ui tests

--- bevy_lint/tests/ui/missing_reflect/derived.rs
+++ bevy_lint/tests/ui/missing_reflect/derived.rs
@@ -1,30 +1,30 @@
-// We want to require annotations for all errors, but don't need them for notes.
-//@require-annotations-for-level: ERROR
+//! Tests the `missing_reflect` lint when `Component`, `Resource`, and `Event` are derived.
 
 #![feature(register_tool)]
 #![register_tool(bevy)]
 #![allow(dead_code)]
+//~v NOTE: the lint level is defined here
 #![deny(bevy::missing_reflect)]
 
 use bevy::prelude::*;
 
 //~v NOTE: `Component` implemented here
 #[derive(Component)]
-#[derive(Reflect)]
-struct MyComponent;
-//~^ ERROR: defined a component without a `Reflect` implementation
 //~| HELP: `Reflect` can be automatically derived
+//~v ERROR: defined a component without a `Reflect` implementation
+struct MyComponent;
 
 //~v NOTE: `Resource` implemented here
 #[derive(Resource)]
-#[derive(Reflect)]
-struct MyResource;
-//~^ ERROR: defined a resource without a `Reflect` implementation
 //~| HELP: `Reflect` can be automatically derived
+//~v ERROR: defined a resource without a `Reflect` implementation
+struct MyResource {
+    field_1: usize,
+    field_2: bool,
+}
 
 //~v NOTE: `Event` implemented here
 #[derive(Event)]
-#[derive(Reflect)]
-struct MyEvent;
-//~^ ERROR: defined an event without a `Reflect` implementation
 //~| HELP: `Reflect` can be automatically derived
+//~v ERROR: defined an event without a `Reflect` implementation
+struct MyEvent(String);
--- bevy_lint/tests/ui/missing_reflect/impl.rs
+++ bevy_lint/tests/ui/missing_reflect/impl.rs
@@ -1,35 +1,36 @@
-//! A version of the normal UI test that replaces `#[derive(...)]` attributes with manual
-//! implementations.
-
-// We want to require annotations for all errors, but don't need them for notes.
-//@require-annotations-for-level: ERROR
+//! Tests the `missing_reflect` lint when `Component`, `Resource`, and `Event` are manually
+//! implemented.
 
 #![feature(register_tool)]
 #![register_tool(bevy)]
 #![allow(dead_code)]
+//~v NOTE: the lint level is defined here
 #![deny(bevy::missing_reflect)]
 
 use bevy::{ecs::component::StorageType, prelude::*};
 
-struct MyComponent;
-//~^ ERROR: defined a component without a `Reflect` implementation
 //~| HELP: `Reflect` can be automatically derived
+//~v ERROR: defined a component without a `Reflect` implementation
+struct MyComponent;
 
 //~v NOTE: `Component` implemented here
 impl Component for MyComponent {
     const STORAGE_TYPE: StorageType = StorageType::Table;
 }
 
-struct MyResource;
-//~^ ERROR: defined a resource without a `Reflect` implementation
 //~| HELP: `Reflect` can be automatically derived
+//~v ERROR: defined a resource without a `Reflect` implementation
+struct MyResource {
+    field_1: usize,
+    field_2: bool,
+}
 
 //~v NOTE: `Resource` implemented here
 impl Resource for MyResource {}
 
-struct MyEvent;
-//~^ ERROR: defined an event without a `Reflect` implementation
 //~| HELP: `Reflect` can be automatically derived
+//~v ERROR: defined an event without a `Reflect` implementation
+struct MyEvent(String);
 
 impl Component for MyEvent {
     const STORAGE_TYPE: StorageType = StorageType::Table;
