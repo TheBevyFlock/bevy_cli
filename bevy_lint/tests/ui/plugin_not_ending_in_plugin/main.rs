db9958abd73caaf306cf4d1961975d6ea9e4b9e7
BD103 <59022059+BD103@users.noreply.github.com>
2024-10-22 08:13:12 -0400

    chore: update tests

--- bevy_lint/tests/ui/plugin_not_ending_in_plugin/main.rs
+++ bevy_lint/tests/ui/plugin_not_ending_in_plugin/main.rs
@@ -1,14 +1,16 @@
 #![feature(register_tool)]
 #![register_tool(bevy)]
 #![deny(bevy::plugin_not_ending_in_plugin)]
+//~^ NOTE: the lint level is defined here
 
 use bevy::prelude::*;
 
 // This should raise an error, since it does not end in "Plugin".
 struct Foo;
-//~^ HELP: rename the plugin
+//~^ ERROR: implemented `Plugin` for a structure whose name does not end in "Plugin"
+//~| HELP: rename the plugin
 
-//~v ERROR: implemented `Plugin` for a structure whose name does not end in "Plugin"
+//~v NOTE: `Plugin` implemented here
 impl Plugin for Foo {
     fn build(&self, _app: &mut App) {}
 }
@@ -20,11 +22,10 @@ impl Plugin for BarPlugin {
     fn build(&self, _app: &mut App) {}
 }
 
-// Though this does not end in "Plugin", the lint is silenced for the `impl` blog, so no error is
-// raised.
+// Though this does not end in "Plugin", the lint is silenced, so no error is raised.
+#[allow(bevy::plugin_not_ending_in_plugin)]
 struct Baz;
 
-#[allow(bevy::plugin_not_ending_in_plugin)]
 impl Plugin for Baz {
     fn build(&self, _app: &mut App) {}
 }
