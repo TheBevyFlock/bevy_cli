b1f6d05e311f8d5d67dd5e3df9a69218322f4acd
BD103 <59022059+BD103@users.noreply.github.com>
2024-11-26 09:24:08 -0500

    feat: improve diagnostics and comments

--- bevy_lint/src/lints/unused_appexit.rs
+++ bevy_lint/src/lints/unused_appexit.rs
@@ -1,6 +1,10 @@
 //! TODO
 
-use clippy_utils::{diagnostics::span_lint, is_expr_used_or_unified, sym, ty::match_type};
+use clippy_utils::{
+    diagnostics::span_lint_and_then, is_expr_used_or_unified, source::snippet_opt, sym,
+    ty::match_type,
+};
+use rustc_errors::Applicability;
 use rustc_hir::{Expr, ExprKind};
 use rustc_lint::{LateContext, LateLintPass};
 use rustc_session::impl_lint_pass;
@@ -17,9 +21,6 @@ declare_bevy_lint! {
 #[derive(Clone, Copy)]
 pub struct UnusedAppExit {
     /// A cached [`Symbol`] representing the interned string `"run"`.
-    ///
-    /// This is used to avoid interning or comparing strings for every single method call in a
-    /// crate.
     run_symbol: Symbol,
 }
 
@@ -41,17 +42,34 @@ impl<'tcx> LateLintPass<'tcx> for UnusedAppExit {
         if let ExprKind::MethodCall(path, src, _, method_span) = expr.kind
             && path.ident.name == self.run_symbol
         {
-            // Get the type of `src` for `src.run()`. We peel away all references because
-            // both `App` and `&mut App` are allowed.
+            // Get the type of `src` for `src.run()`. We peel away all references because both
+            // `App` and `&mut App` are allowed.
             let ty = cx.typeck_results().expr_ty(src).peel_refs();
 
             // If `src` is a Bevy `App` and the returned `AppExit` is not used, emit the lint.
             if match_type(cx, ty, &crate::paths::APP) && !is_expr_used_or_unified(cx.tcx, expr) {
-                span_lint(
+                span_lint_and_then(
                     cx,
                     UNUSED_APPEXIT.lint,
                     method_span,
                     UNUSED_APPEXIT.lint.desc,
+                    |diag| {
+                        diag.note("`App::run()` returns `AppExit`, which is used to determine whether the app exited successfully or not");
+                        diag.help("`AppExit` implements `Termination`, so it can be returned directly from `fn main()`");
+
+                        if let Some(snippet) = snippet_opt(cx, expr.span) {
+                            diag.span_suggestion(
+                                expr.span,
+                                "try",
+                                format!("let _app_exit = {snippet};"),
+                                // I believe this should always be machine applicable. We are
+                                // matching an expression, which is always matched in the grammar
+                                // for `let` statements.
+                                // See <https://doc.rust-lang.org/reference/statements.html#let-statements>.
+                                Applicability::MachineApplicable,
+                            );
+                        }
+                    },
                 );
             }
         }
