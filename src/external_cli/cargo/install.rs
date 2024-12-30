e69756db273297899951c2f46a8c0ce44f1a1dcc
DaAlbrecht <dadidi42@gmail.com>
2024-12-30 17:07:46 +0100

    feat: check if installed `wasm-bindgen-cli` version matches the desired version otherwise prompt to install and replace

--- src/external_cli/cargo/install.rs
+++ src/external_cli/cargo/install.rs
@@ -1,18 +1,24 @@
-use std::process::{exit, Command};
+use std::{
+    process::{exit, Command},
+    str::FromStr,
+};
 
 use dialoguer::Confirm;
+use semver::Version;
+
+use crate::external_cli::wasm_bindgen;
+
+use self::wasm_bindgen::wasm_bindgen_cli_version;
 
 /// Check if the given program is installed on the system.
 ///
 /// This assumes that the program offers a `--version` flag.
-fn is_installed(program: &str) -> bool {
-    let output = Command::new(program).arg("--version").output();
-
-    if let Ok(output) = output {
-        output.status.success()
-    } else {
-        false
-    }
+fn is_installed(program: &str) -> Option<Vec<u8>> {
+    Command::new(program)
+        .arg("--version")
+        .output()
+        .map(|output| output.stdout)
+        .ok()
 }
 
 /// Checks if the program is installed and installs it if it isn't.
@@ -25,16 +31,37 @@ pub(crate) fn if_needed(
     ask_user: bool,
     hidden: bool,
 ) -> anyhow::Result<bool> {
-    if is_installed(program) {
-        return Ok(false);
+    let mut prompt: Option<String> = None;
+
+    if let Some(stdout) = is_installed(program) {
+        let Some(package_version) = package_version else {
+            // If no `package_version` is specified and the program is installed,
+            // there is nothing to do.
+            return Ok(false);
+        };
+        // If `package_version` is specified, check if the installed version matches the desired
+        //`package_version`
+        if package == wasm_bindgen::PACKAGE {
+            let version = wasm_bindgen_cli_version(stdout)?;
+            let desired_version = Version::from_str(package_version)?;
+            if version == desired_version {
+                return Ok(false);
+            }
+            prompt = Some(format!(
+                "`{program}:{version}` is installed, but \
+                version `{desired_version}` is required. Install and replace?"
+            ));
+        }
     }
 
     // Abort if the user doesn't want to install it
     if ask_user
         && !Confirm::new()
-            .with_prompt(format!(
-                "`{program}` is missing, should I install it for you?"
-            ))
+            .with_prompt(
+                prompt.unwrap_or_else(|| {
+                    format!("`{program}` is missing, should I install it for you?")
+                }),
+            )
             .interact()?
     {
         exit(1);
--- src/external_cli/wasm_bindgen.rs
+++ src/external_cli/wasm_bindgen.rs
@@ -1,4 +1,5 @@
-use std::process::Command;
+use semver::Version;
+use std::{process::Command, str::FromStr};
 
 use crate::{external_cli::CommandHelpers, run::BinTarget};
 
@@ -26,4 +27,33 @@ pub(crate) fn bundle(bin_target: &BinTarget) -> anyhow::Result<()> {
         .ensure_status()?;
 
     Ok(())
+}
+
+/// Transforms the output from `wasm-bindgen --version` into a [Version].
+///
+/// # Errors
+///
+/// * The `stdout` is not valid UTF-8.
+/// * The output does not match the expected format: `wasm-bindgen <version>`.
+/// * The version string cannot be parsed into a `semver::Version`.
+///
+/// # Examples
+/// ```
+/// let stdout = b"wasm-bindgen 0.2.99".to_vec();
+/// let version = wasm_bindgen_cli_version(stdout).unwrap();
+/// assert_eq!(version, Version::new(0, 2, 99));
+/// ```
+pub(crate) fn wasm_bindgen_cli_version(stdout: Vec<u8>) -> anyhow::Result<Version> {
+    let stdout = String::from_utf8(stdout)?;
+    // Example stdout from `wasm-bindgen --version`: wasm-bindgen 0.2.99
+    stdout
+        .split_whitespace()
+        .nth(1)
+        .ok_or_else(|| {
+            anyhow::anyhow!(
+                "unexpected output format: {}, expected format to be: `wasm-bindgen <version>`",
+                stdout
+            )
+        })
+        .and_then(|version| Version::from_str(version).map_err(|e| anyhow::anyhow!(e)))
 }
