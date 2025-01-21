e006ef32245c616ef32e44c887af90b84b2b91a9
BD103 <59022059+BD103@users.noreply.github.com>
2025-01-21 14:30:03 -0500

    feat: register "bevy" as a default tool when the linter is run

--- bevy_lint/src/callback.rs
+++ bevy_lint/src/callback.rs
@@ -1,5 +1,23 @@
+use std::sync::atomic::{AtomicPtr, Ordering};
+
 use rustc_driver::Callbacks;
 use rustc_interface::interface::Config;
+use rustc_lint_defs::RegisteredTools;
+use rustc_middle::ty::TyCtxt;
+use rustc_span::Ident;
+
+/// A pointer to the original [`registered_tools()`](TyCtxt::registered_tools) query function.
+///
+/// # Safety
+///
+/// This pointer must be of the type [`fn(TyCtxt<'tcx>, ()) -> RegisteredTools`](fn).
+static ORIGINAL_REGISTERED_TOOLS: AtomicPtr<()> = {
+    fn default(_: TyCtxt<'_>, _: ()) -> RegisteredTools {
+        unreachable!("This function will be overwritten when `BevyLintCallback::config()` is run.");
+    }
+
+    AtomicPtr::new(default as *mut ())
+};
 
 /// The `rustc` [`Callbacks`] that register Bevy's lints.
 pub struct BevyLintCallback;
@@ -20,5 +38,33 @@ impl Callbacks for BevyLintCallback {
             crate::lints::register_passes(store);
             crate::groups::register_groups(store);
         }));
+
+        config.override_queries = Some(|_session, providers| {
+            // Save the original query so we can access it later.
+            ORIGINAL_REGISTERED_TOOLS.store(
+                providers.queries.registered_tools as *mut (),
+                Ordering::Relaxed,
+            );
+
+            // Overwrite the query with our own custom version.
+            providers.queries.registered_tools = registered_tools;
+        });
     }
+}
+
+/// A custom version of the [`registered_tools()`](TyCtxt::registered_tools) query that
+/// automatically adds "bevy" as a tool.
+fn registered_tools<'tcx>(tcx: TyCtxt<'tcx>, _: ()) -> RegisteredTools {
+    // Fetch the original version of the query.
+    //
+    // SAFETY: The pointer is guaranteed to be a `fn(TyCtxt<'tcx>, ()) -> RegisteredTools` as per
+    // `ORIGINAL_REGISTERED_TOOLS`'s safety docs.
+    let original: fn(TyCtxt<'tcx>, ()) -> RegisteredTools =
+        unsafe { std::mem::transmute(ORIGINAL_REGISTERED_TOOLS.load(Ordering::Relaxed)) };
+
+    let mut tools = (original)(tcx, ());
+
+    tools.insert(Ident::from_str("bevy"));
+
+    tools
 }
