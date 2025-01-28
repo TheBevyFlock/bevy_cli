1c5a9b507d55981a967addbd0f47c284d5995e3d
BD103 <59022059+BD103@users.noreply.github.com>
2025-01-28 10:01:27 -0500

    feat: update ui test to use more nested tuples

--- bevy_lint/tests/ui/insert_unit_bundle/main.rs
+++ bevy_lint/tests/ui/insert_unit_bundle/main.rs
@@ -6,26 +6,31 @@ use bevy::prelude::*;
 use std::f32::consts::PI;
 
 fn main() {
-    App::new().add_systems(Startup, test);
+    App::new().add_systems(Startup, my_system);
 }
 
-fn test(mut commands: Commands) {
+fn my_system(mut commands: Commands) {
+    commands.spawn(());
+    //~^ ERROR: inserted a `Bundle` containing a unit `()` type
+
     commands.spawn((
         Name::new("Decal"),
         Transform::from_translation(Vec3::new(0.75, 0.0, 0.0)).rotate_z(PI / 4.0),
         //~^ ERROR: inserted a `Bundle` containing a unit `()` type
-        example(),
-        //~^ ERROR: inserted a `Bundle` containing a unit `()` type
-        (),
-        //~^ ERROR: inserted a `Bundle` containing a unit `()` type
-        {
-            example();
-
-            Transform::from_translation(Vec3::ONE)
-        },
+        (
+            no_op(),
+            //~^ ERROR: inserted a `Bundle` containing a unit `()` type
+            GlobalTransform::IDENTITY,
+            (
+                (),
+                //~^ ERROR: inserted a `Bundle` containing a unit `()` type
+            ),
+            {
+                no_op();
+                Transform::default()
+            },
+        ),
     ));
 }
 
-fn example() {
-    let foo = 1;
-}
+fn no_op() {}
