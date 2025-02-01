404057ac3f80c3420742faa8d2422e12bc1fadca
BD103 <59022059+BD103@users.noreply.github.com>
2025-01-31 21:23:33 -0500

    `Cargo.toml` linter configuration (#251)
    # Objective
    
    - Setting default lint levels within code is tedious and requires lots
    of boilerplate, as you must do it once per crate-root and must detect
    when the linter is being run. (See [Toggling
    Lints](https://github.com/TheBevyFlock/bevy_cli/blob/d3e79396595f675798af5be8be56efbfd2737ba0/bevy_lint/README.md#toggling-lints).)
    
    ## Solution
    
    - Support loading lint level configuration from `Cargo.toml` using the
    `[package.metadata.bevy_lint]` and `[workspace.metadata.bevy_lint]`
    tables.
    - This is modeled off of Cargo's `[lints]` table, although there are a
    few minor differences.
    - Closes #113.
    
    ## Examples
    
    ```toml
    # A crate-level `Cargo.toml`.
    [package]
    name = "foo"
    edition = "2021"
    
    [package.metadata.bevy_lint]
    missing_reflect = "warn"
    panicking_methods = { level = "forbid" }
    ```
    
    ```toml
    # Workspace `Cargo.toml`.
    [workspace]
    members = ["crates/*"]
    resolver = "2"
    
    [workspace.metadata.bevy_lint]
    pedantic = { level = "warn" }
    panicking_methods = "warn"
    ```
    
    Lint level configuration is merged between the crate and workspace
    `Cargo.toml`, with the crate level taking priority. When linting the
    crate `foo`, as mentioned above, the linter will set the levels for the
    following lints to:
    
    ```toml
    # Merged from crate.
    missing_reflect = "warn"
    # Was "warn" in workspace, but overridden by crate.
    panicking_methods = "forbid"
    # Merged from workspace.
    pedantic = "warn"
    ```
    
    ## Internal Explanation
    
    Internally, lint level configuration works by:
    
    1. Reading the crate and workspace `Cargo.toml`s and merging the
    configuration together.
    2. Adding the paths to both `Cargo.toml`s to
    [`ParseSess::file_depinfo`](https://doc.rust-lang.org/nightly/nightly-rustc/rustc_session/parse/struct.ParseSess.html#structfield.file_depinfo)
    so that the linter will re-run if either `Cargo.toml` is modified.
    3. Registering `bevy` as a tool (just like `#![register_tool(bevy)]`!)
    by overriding `TyCtxt::registered_tools()`.
    4. Inserting `--warn bevy::lint --allow bevy::other-lint` flags into the
    compiler options.
    - Registering `bevy` as a tool is required for the compiler to accept
    `--deny bevy::lint` flags. We can do it internally, but only when
    `bevy_lint` is being run.
    
    # Future Work
    
    I originally intended to support generic linter configuration, meaning
    that lints could have custom properties in `Cargo.toml` that they would
    read, but got stuck on a few issues with my design. I reverted my work
    on that in 1a7578a5f6618de6f24a7fe10425827a6a7706df, but intend to
    approach it again in the future.

---
+++ bevy_lint/src/config.rs
@@ -0,0 +1,153 @@
+use std::sync::RwLock;
+
+use rustc_interface::Config;
+use rustc_lint::Level;
+use rustc_session::utils::was_invoked_from_cargo;
+use serde::Deserialize;
+use toml::{Table, Value};
+
+static LINTER_CONFIG: RwLock<Option<Table>> = RwLock::new(None);
+
+/// Loads the configuration from the crate and workspace-level `Cargo.toml`s.
+///
+/// This is the main entrypoint that sets up configuration. It is responsible for reading
+/// `Cargo.toml`, merging the configuration together, setting the default lint levels based on that
+/// configuration, and storing the config so it can be accessed later on.
+pub fn load_config(compiler_config: &mut Config) {
+    let mut linter_config = LINTER_CONFIG.write().unwrap();
+
+    // Clear the config, if any exists. This prevents old values from leaking into new sessions,
+    // in the case we cannot load the configuration.
+    *linter_config = None;
+
+    // Configuration is read from `Cargo.toml`. If we're not being called from Cargo, we should
+    // avoid assuming the configuration in `Cargo.toml` is desired by the user.
+    if !was_invoked_from_cargo() {
+        return;
+    }
+
+    // Load the linter configuration for both the workspace and crate `Cargo.toml`s.
+    let workspace_config = load_cargo_manifest(compiler_config, true)
+        .and_then(|manifest| deserialize_linter_config(&manifest, true));
+
+    let crate_config = load_cargo_manifest(compiler_config, false)
+        .and_then(|manifest| deserialize_linter_config(&manifest, false));
+
+    let config = match (workspace_config, crate_config) {
+        // If only one of the configs are specified, just use that.
+        (Some(config), None) | (None, Some(config)) => config,
+        // If both configs are specified, merge them.
+        (Some(workspace_config), Some(crate_config)) => {
+            merge_linter_configs(workspace_config, crate_config)
+        }
+        // If neither of the configs are specified, exit.
+        (None, None) => return,
+    };
+
+    insert_lint_levels(compiler_config, &config);
+
+    *linter_config = Some(config);
+}
+
+/// Returns the contents of `Cargo.toml` associated with the crate being compiled.
+///
+/// This will return [`None`] if `Cargo.toml` cannot be located or read. If `workspace` is true,
+/// this will instead return the workspace `Cargo.toml`.
+fn load_cargo_manifest(compiler_config: &Config, workspace: bool) -> Option<String> {
+    crate::utils::cargo::locate_manifest(&compiler_config.input, workspace)
+        .and_then(std::fs::read_to_string)
+        .ok()
+}
+
+/// Returns the [`Table`] representing `[package.metadata.bevy_lint]` given the string contents of
+/// `Cargo.toml`.
+///
+/// This will return [`None`] if [`toml`] cannot deserialize the `manifest`, or if
+/// `[package.metadata.bevy_lint]` is not specified. If `workspace` is true, this will instead
+/// return the [`Table`] for `[workspace.metadata.bevy_lint]`.
+fn deserialize_linter_config(manifest: &str, workspace: bool) -> Option<Table> {
+    /// Represents `Cargo.toml` in the following format:
+    ///
+    /// ```toml
+    /// [package.metadata.bevy_lint]
+    /// lint_name = "level"
+    /// other_lint_name = { level = "level", foo = 8, bar = false }
+    ///
+    /// [workspace.metadata.bevy_lint]
+    /// yet_another_lint_name = "level"
+    /// ```
+    #[derive(Deserialize)]
+    struct Manifest {
+        package: Option<PackageOrWorkspace>,
+        workspace: Option<PackageOrWorkspace>,
+    }
+
+    #[derive(Deserialize)]
+    struct PackageOrWorkspace {
+        metadata: Option<Metadata>,
+    }
+
+    #[derive(Deserialize)]
+    struct Metadata {
+        bevy_lint: Option<Table>,
+    }
+
+    toml::from_str::<Manifest>(manifest)
+        .ok()
+        .and_then(|manifest| {
+            if workspace {
+                manifest.workspace
+            } else {
+                manifest.package
+            }
+        })
+        .and_then(|package_or_workspace| package_or_workspace.metadata)
+        .and_then(|metadata| metadata.bevy_lint)
+}
+
+/// Merges the [`Table`]s for the workspace and crate linter configs together.
+///
+/// The crate `Cargo.toml` takes precedence, so its configuration will overwrite the workspace's
+/// configuration.
+fn merge_linter_configs(mut workspace_config: Table, crate_config: Table) -> Table {
+    for (lint_name, lint_config) in crate_config {
+        // `Map::insert()` overwrite an existing value with a new one, if one already existed.
+        workspace_config.insert(lint_name, lint_config);
+    }
+
+    workspace_config
+}
+
+/// Informs the compiler of `Cargo.toml` lint level configuration.
+///
+/// This function inserts `--warn`, `--allow`, `--deny`, and more flags into the compiler
+/// [`Config`] to set lint levels. For example:
+///
+/// ```toml
+/// [package.metadata.bevy_lint]
+/// # Inserts `--warn bevy::missing_reflect`.
+/// missing_reflect = "warn"
+/// # Inserts `--allow bevy::insert_event_resource`.
+/// insert_event_resource = { level = "allow" }
+/// ```
+fn insert_lint_levels(compiler_config: &mut Config, linter_config: &Table) {
+    for (lint_name, lint_config) in linter_config {
+        let level = match lint_config {
+            // The format of `lint_name = "level"`.
+            Value::String(s) => Level::from_str(s),
+            // The format of `lint_name = { level = "level" }`.
+            Value::Table(table) => table
+                .get("level")
+                .and_then(Value::as_str)
+                .and_then(Level::from_str),
+            _ => None,
+        };
+
+        if let Some(level) = level {
+            compiler_config
+                .opts
+                .lint_opts
+                .push((format!("bevy::{lint_name}"), level));
+        }
+    }
+}
