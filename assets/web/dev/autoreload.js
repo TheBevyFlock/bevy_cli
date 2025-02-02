c041a25ed23a1a1392560af18ccbd50ecdaf4854
Tim Jentzsch <commits@timjen.net>
2025-02-02 14:24:21 +0100

    Add script for automatic reloading

---
+++ assets/web/dev/autoreload.js
@@ -0,0 +1,36 @@
+// Automatically reload when the server restarts
+// Adopted from <https://github.com/trunk-rs/trunk/pull/231>
+(function () {
+  const url = 'ws://' + window.location.host + '/_bevy_dev/ws';
+  const pollIntervalMs = 5_000;
+  let webSocket;
+
+  const reload_upon_connect = () => {
+    window.setTimeout(
+      () => {
+        // Reload when reconnecting to the websocket, e.g. because the server has been restarted
+        webSocket = new WebSocket(url);
+        webSocket.onopen = () => window.location.reload();
+        webSocket.onclose = reload_upon_connect;
+      },
+      pollIntervalMs);
+  };
+
+  webSocket = new WebSocket(url);
+  webSocket.onmessage = (ev) => {
+    try {
+      const msg = JSON.parse(ev.data);
+
+      switch (msg.type) {
+        case "reload":
+          window.location.reload();
+        default:
+          console.warn("Unknown websocket message", msg);
+      }
+    } catch (error) {
+      console.warn("Failed to parse websocket message", error);
+    }
+  };
+
+  webSocket.onclose = reload_upon_connect;
+})()
