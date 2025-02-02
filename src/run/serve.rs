d446b6d16e3a920cae8dacace34a768b44ea0313
Tim Jentzsch <commits@timjen.net>
2025-02-02 14:39:27 +0100

    Serve the auto reload script

--- src/run/serve.rs
+++ src/run/serve.rs
@@ -14,6 +14,17 @@ async fn serve_static_html(content: &'static str) -> impl Responder {
         .body(content)
 }
 
+/// Serve a static JavaScript file with the given content.
+async fn serve_static_js(content: &'static str) -> impl Responder {
+    // Build the HTTP response with appropriate headers to serve the content as a file
+    HttpResponse::Ok()
+        .insert_header((
+            actix_web::http::header::CONTENT_TYPE,
+            "text/javascript; charset=utf-8",
+        ))
+        .body(content)
+}
+
 /// Launch a web server running the Bevy app.
 pub(crate) fn serve(web_bundle: WebBundle, port: u16) -> anyhow::Result<()> {
     rt::System::new().block_on(
@@ -33,16 +44,27 @@ pub(crate) fn serve(web_bundle: WebBundle, port: u16) -> anyhow::Result<()> {
                 }) => {
                     // Serve the build artifacts at the `/build/*` route
                     // A custom `index.html` will have to call `/build/{bin_name}.js`
-                    app = app.service(
-                        actix_files::Files::new("/build", build_artifact_path)
-                            // This potentially includes artifacts which we will not need,
-                            // but we can't add the bin name to the check due to lifetime
-                            // requirements
-                            .path_filter(move |path, _| {
-                                path.file_name() == Some(&js_file_name)
-                                    || path.file_name() == Some(&wasm_file_name)
+                    app = app
+                        .service(
+                            actix_files::Files::new("/build", build_artifact_path)
+                                // This potentially includes artifacts which we will not need,
+                                // but we can't add the bin name to the check due to lifetime
+                                // requirements
+                                .path_filter(move |path, _| {
+                                    path.file_name() == Some(&js_file_name)
+                                        || path.file_name() == Some(&wasm_file_name)
+                                }),
+                        )
+                        // Serve the script to automatically reload the page on changes
+                        .route(
+                            "/_bevy_dev/auto_reload.js",
+                            web::get().to(move || {
+                                serve_static_js(include_str!(concat!(
+                                    env!("CARGO_MANIFEST_DIR"),
+                                    "/assets/web/_bevy_dev/auto_reload.js"
+                                )))
                             }),
-                    );
+                        );
 
                     // If the app has an assets folder, serve it under `/assets`
                     if let Some(assets_path) = assets_path {
