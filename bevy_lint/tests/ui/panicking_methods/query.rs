2668cbabca0b8dbbfec55fda1f445dbce8edc868
DAA <42379074+DaAlbrecht@users.noreply.github.com>
2025-02-20 18:06:19 +0100

    Merge `panicking_query_methods` and `panicking_world_methods` (#271)
    Closes #269

--- bevy_lint/tests/ui/panicking_methods/query.rs
+++ bevy_lint/tests/ui/panicking_methods/query.rs
@@ -1,8 +1,8 @@
-//! This tests the `panicking_query_methods` lint, specifically when triggered on the `Query` type.
+//! This tests the `panicking_methods` lint, specifically when triggered on the `Query` type.
 
 #![feature(register_tool)]
 #![register_tool(bevy)]
-#![deny(bevy::panicking_query_methods)]
+#![deny(bevy::panicking_methods)]
 
 use bevy::prelude::*;
 
