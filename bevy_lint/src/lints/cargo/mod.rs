f2ba67b89e8f5977740e21d854b649a510f64990
DaAlbrecht <dadidi42@gmail.com>
2025-02-21 15:16:45 +0100

    feat: remove unused import

--- bevy_lint/src/lints/cargo/mod.rs
+++ bevy_lint/src/lints/cargo/mod.rs
@@ -1,6 +1,6 @@
 use crate::{declare_bevy_lint, declare_bevy_lint_pass};
 use cargo_metadata::MetadataCommand;
-use clippy_utils::{diagnostics::span_lint, sym};
+use clippy_utils::sym;
 use rustc_lint::{LateContext, LateLintPass};
 use rustc_session::{config::Input, utils::was_invoked_from_cargo};
 use rustc_span::{BytePos, Pos, SourceFile, Span, Symbol, SyntaxContext};
