6111e969fac4842f43756cc48299cca0fe9a26bd
Christopher Biscardi <chris@christopherbiscardi.com>
2025-02-27 20:45:27 -0800

    switch to axum

--- src/run/serve.rs
+++ src/run/serve.rs
@@ -1,133 +1,121 @@
 //! Serving the app locally for the browser.
-use actix_web::{rt, web, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
-use actix_ws::AggregatedMessage;
-use futures_util::StreamExt as _;
+use std::net::SocketAddr;
+use axum::{
+    extract::{
+        ws::{Message, WebSocket},
+        WebSocketUpgrade,
+    },
+    response::Response,
+    routing::{any, get},
+    Router,
+};
+use tower_http::{
+    services::{ServeDir, ServeFile},
+    trace::TraceLayer,
+};
 
 use crate::web::bundle::{Index, LinkedBundle, PackedBundle, WebBundle};
 
-/// Serve a static HTML file with the given content.
-async fn serve_static_html(content: &'static str) -> impl Responder {
-    // Build the HTTP response with appropriate headers to serve the content as a file
-    HttpResponse::Ok()
-        .insert_header((
-            actix_web::http::header::CONTENT_TYPE,
-            "text/html; charset=utf-8",
-        ))
-        .body(content)
+async fn dev_websocket(ws: WebSocketUpgrade) -> Response {
+    ws.on_upgrade(handle_socket)
 }
 
-/// Serve a static JavaScript file with the given content.
-async fn serve_static_js(content: &'static str) -> impl Responder {
-    // Build the HTTP response with appropriate headers to serve the content as a file
-    HttpResponse::Ok()
-        .insert_header((
-            actix_web::http::header::CONTENT_TYPE,
-            "text/javascript; charset=utf-8",
-        ))
-        .body(content)
-}
-
-/// Handle the websocket connection to the client in dev mode.
-async fn dev_websocket(req: HttpRequest, stream: web::Payload) -> Result<HttpResponse, Error> {
-    let (res, mut session, stream) = actix_ws::handle(&req, stream)?;
-
-    let mut stream = stream
-        .aggregate_continuations()
-        // aggregate continuation frames up to 1MiB
-        .max_continuation_size(2_usize.pow(20));
-
-    rt::spawn(async move {
-        // receive messages from websocket
-        while let Some(msg) = stream.next().await {
-            if let Ok(AggregatedMessage::Text(text)) = msg {
-                // echo text message
-                session.text(text).await.unwrap();
+async fn handle_socket(mut socket: WebSocket) {
+    while let Some(Ok(msg)) = socket.recv().await {
+        if let Message::Text(msg) = msg {
+            if socket
+                .send(Message::Text(format!("You said: {msg}").into()))
+                .await
+                .is_err()
+            {
+                break;
             }
         }
-    });
-
-    // respond immediately with response connected to WS session
-    Ok(res)
+    }
 }
 
 /// Launch a web server running the Bevy app.
-pub(crate) fn serve(web_bundle: WebBundle, port: u16) -> anyhow::Result<()> {
-    rt::System::new().block_on(
-        HttpServer::new(move || {
-            let mut app = App::new();
+#[tokio::main]
+pub(crate) async fn serve(web_bundle: WebBundle, port: u16) -> anyhow::Result<()> {
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+    let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
+    tracing::info!("listening on {}", listener.local_addr().unwrap());
 
-            match web_bundle.clone() {
-                WebBundle::Packed(PackedBundle { path }) => {
-                    app = app.service(actix_files::Files::new("/", path).index_file("index.html"));
-                }
-                WebBundle::Linked(LinkedBundle {
-                    build_artifact_path,
-                    wasm_file_name,
-                    js_file_name,
-                    index,
-                    assets_path,
-                }) => {
-                    // Serve the build artifacts at the `/build/*` route
-                    // A custom `index.html` will have to call `/build/{bin_name}.js`
-                    app = app
-                        .service(
-                            actix_files::Files::new("/build", build_artifact_path)
-                                // This potentially includes artifacts which we will not need,
-                                // but we can't add the bin name to the check due to lifetime
-                                // requirements
-                                .path_filter(move |path, _| {
-                                    path.file_name() == Some(&js_file_name)
-                                        || path.file_name() == Some(&wasm_file_name)
-                                }),
+    let mut router = Router::new().layer(TraceLayer::new_for_http());
+
+    match web_bundle.clone() {
+        WebBundle::Packed(PackedBundle { path }) => {
+            tracing::info!(?path, "packed");
+            router = router.route_service(
+                &format!("/{}", path.display()),
+                ServeFile::new("index.html"),
+            );
+        }
+        WebBundle::Linked(LinkedBundle {
+            build_artifact_path,
+            wasm_file_name,
+            js_file_name,
+            index,
+            assets_path,
+        }) => {
+            router = router
+                .route_service(
+                    &format!("/build/{}", js_file_name.to_str().unwrap()),
+                    ServeFile::new(build_artifact_path.join(js_file_name)),
+                )
+                .route_service(
+                    &format!("/build/{}", wasm_file_name.to_str().unwrap()),
+                    ServeFile::new(build_artifact_path.join(wasm_file_name)),
+                )
+                .route(
+                    "/_bevy_dev/auto_reload.js",
+                    get(async || {
+                        (
+                            [(http::header::CONTENT_TYPE, "text/javascript; charset=utf-8")],
+                            include_str!(concat!(
+                                env!("CARGO_MANIFEST_DIR"),
+                                "/assets/web/_bevy_dev/auto_reload.js"
+                            )),
                         )
-                        // Serve the script to automatically reload the page on changes
-                        .route(
-                            "/_bevy_dev/auto_reload.js",
-                            web::get().to(move || {
-                                serve_static_js(include_str!(concat!(
-                                    env!("CARGO_MANIFEST_DIR"),
-                                    "/assets/web/_bevy_dev/auto_reload.js"
-                                )))
-                            }),
-                        )
-                        // Open a websocket for automatic reloading
-                        // For now, just echo the messages back
-                        .route("/_bevy_dev/websocket", web::get().to(dev_websocket));
+                    }),
+                )
+                // Open a websocket for automatic reloading
+                // For now, just echo the messages back
+                .route("/_bevy_dev/websocket", any(dev_websocket));
 
-                    // If the app has an assets folder, serve it under `/assets`
-                    if let Some(assets_path) = assets_path {
-                        app = app.service(actix_files::Files::new("/assets", assets_path))
-                    }
-
-                    match index {
-                        Index::File(path) => {
-                            app = app.service(
-                                actix_files::Files::new("/", path).index_file("index.html"),
-                            );
-                        }
-                        Index::Content(content) => {
-                            // Try to inject the auto reload script in the document body
-                            // TODO: Do this also for the other cases when the `index.html` is in a
-                            // folder
-                            let contents = content.replace(
-                                "</body>",
-                                r#"<script src="_bevy_dev/auto_reload.js"></script></body>"#,
-                            );
-
-                            // PERF: We have to leak the string to get a static lifetime
-                            // But this will only be done once so it should be fine for memory
-                            let contents: &'static str = Box::leak(contents.into_boxed_str());
-                            app = app.route("/", web::get().to(move || serve_static_html(contents)))
-                        }
-                    }
-                }
+            // If the app has an assets folder, serve it under `/assets`
+            if let Some(assets_path) = assets_path {
+                router = router.nest_service("/assets", ServeDir::new(assets_path));
             }
 
-            app
-        })
-        .bind(("127.0.0.1", port))?
-        .run(),
-    )?;
+            match index {
+                Index::File(path) => {
+                    router = router.route_service("/", ServeDir::new(path));
+                }
+                Index::Content(content) => {
+                    // Try to inject the auto reload script in the document body
+                    // TODO: Do this also for the other cases when the `index.html` is in a
+                    // folder
+                    let contents = content.replace(
+                        "</body>",
+                        r#"<script src="_bevy_dev/auto_reload.js"></script></body>"#,
+                    );
+
+                    router = router.route(
+                        "/",
+                        get(async || {
+                            (
+                                [(http::header::CONTENT_TYPE, "text/html; charset=utf-8")],
+                                contents,
+                            )
+                        }),
+                    );
+                }
+            }
+        }
+    }
+
+    axum::serve(listener, router).await.unwrap();
 
     Ok(())
 }
