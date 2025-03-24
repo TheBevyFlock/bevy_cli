dafbae195cd6146ae54c9ca19a9429133e7742b3
BD103 <59022059+BD103@users.noreply.github.com>
2025-03-24 10:04:40 -0400

    fix: use the adjusted type

--- bevy_lint/src/lints/iter_current_update_events.rs
+++ bevy_lint/src/lints/iter_current_update_events.rs
@@ -24,9 +24,24 @@ declare_bevy_lint_pass! {
 impl<'tcx> LateLintPass<'tcx> for IterCurrentUpdateEvents {
     fn check_expr(&mut self, cx: &LateContext<'tcx>, expr: &'tcx Expr<'tcx>) {
         if let Some(method_call) = MethodCall::try_from(cx, expr) {
+            // Find the adjusted type of the receiver. Type adjustment does things like
+            // auto-dereference and type coercion. In this example, we use the adjusted type so
+            // that we can also handle `Res<Events<T>>`.
+            //
+            // ```
+            // fn plain(events: Events<T>) {
+            //     // Original type is `Events<T>`, adjusted type is `Events<T>`.
+            //     let _ = events.iter_current_update_events();
+            // }
+            //
+            // fn res(events: Res<Events<T>>) {
+            //     // Original type is `Res<Events<T>>`, adjusted type is `Events<T>`.
+            //     let _ = events.iter_current_update_events();
+            // }
+            // ```
             let src_ty = cx
                 .typeck_results()
-                .expr_ty(method_call.receiver)
+                .expr_ty_adjusted(method_call.receiver)
                 .peel_refs();
 
             if !match_type(cx, src_ty, &crate::paths::EVENTS) {
