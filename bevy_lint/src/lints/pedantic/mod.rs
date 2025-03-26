1b3dd8ae6715e1a46461aa249aa3a03d3b6cb156
BD103 <59022059+BD103@users.noreply.github.com>
2025-03-26 10:55:47 -0400

    feat: add docs to lint group modules

--- bevy_lint/src/lints/pedantic/mod.rs
+++ bevy_lint/src/lints/pedantic/mod.rs
@@ -1,2 +1,6 @@
+//! Lints that make the linter incredibly nit-picky.
+//!
+//! For more information, please see [`PEDANTIC`](crate::groups::PEDANTIC).
+
 pub mod borrowed_reborrowable;
 pub mod main_return_without_appexit;
--- bevy_lint/src/lints/suspicious/mod.rs
+++ bevy_lint/src/lints/suspicious/mod.rs
@@ -1,3 +1,8 @@
+//! Lints similar to [`correctness`](super::correctness) that checks for suspicious or usually
+//! wrong code.
+//!
+//! For more information, please see [`SUSPICIOUS`](crate::groups::SUSPICIOUS).
+
 pub mod insert_event_resource;
 pub mod insert_unit_bundle;
 pub mod iter_current_update_events;
