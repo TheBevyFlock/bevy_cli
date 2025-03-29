c05ac1982278220a4fb5bf031546fc75e7916386
DAA <42379074+DaAlbrecht@users.noreply.github.com>
2025-03-29 17:50:10 +0100

    Use direct field access instead of `HasSession` trait (#326)
    Unifies how we access the `Session`. Noticed I sometimes used the method
    to access it after reading
    https://github.com/TheBevyFlock/bevy_cli/issues/325

--- bevy_lint/src/lints/style/plugin_not_ending_in_plugin.rs
+++ bevy_lint/src/lints/style/plugin_not_ending_in_plugin.rs
@@ -38,9 +38,7 @@
 //! ```
 
 use crate::{declare_bevy_lint, declare_bevy_lint_pass};
-use clippy_utils::{
-    diagnostics::span_lint_hir_and_then, match_def_path, path_res, source::HasSession,
-};
+use clippy_utils::{diagnostics::span_lint_hir_and_then, match_def_path, path_res};
 use rustc_errors::Applicability;
 use rustc_hir::{HirId, Item, ItemKind, OwnerId, def::Res};
 use rustc_lint::{LateContext, LateLintPass};
@@ -98,7 +96,7 @@ impl<'tcx> LateLintPass<'tcx> for PluginNotEndingInPlugin {
             };
 
             // skip lint if the struct was defined in an external macro
-            if struct_span.in_external_macro(cx.tcx.sess().source_map()) {
+            if struct_span.in_external_macro(cx.tcx.sess.source_map()) {
                 return;
             }
 
