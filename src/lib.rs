8e33f9b53c3338100119678ec6797b0deb703889
Tim Jentzsch <commits@timjen.net>
2025-03-30 23:26:46 +0200

    Fix formatting

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,10 @@
 //! The library backend for the Bevy CLI.
 
 pub mod build;
+pub mod config;
 pub mod external_cli;
 pub mod lint;
 pub mod run;
 pub mod template;
 #[cfg(feature = "web")]
 pub(crate) mod web;
-pub mod config;
