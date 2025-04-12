594812cb77e12f52312b033ce9eab6da4ad21f2a
BD103 <59022059+BD103@users.noreply.github.com>
2025-04-12 09:31:33 -0400

    chore: remove unused `LINTS` and `GROUPS` statics

--- bevy_lint/src/lints/mod.rs
+++ bevy_lint/src/lints/mod.rs
@@ -22,8 +22,8 @@
 //!
 //! [lint groups that can be toggled together]: crate#toggling-lints-in-cargotoml
 
-use crate::lint::{BevyLint, LintGroup, LintGroup2};
-use rustc_lint::{Lint, LintStore};
+use crate::lint::LintGroup2;
+use rustc_lint::LintStore;
 
 mod cargo;
 
@@ -35,38 +35,6 @@ pub mod performance;
 pub mod restriction;
 pub mod style;
 pub mod suspicious;
-
-/// A list of all [`BevyLint`]s.
-///
-/// If a group is not in this list, it will not be registered in [`register_lints()`].
-static LINTS: &[&BevyLint] = &[
-    // This list should be sorted alphabetically based on the lint's name, not group.
-    pedantic::borrowed_reborrowable::BORROWED_REBORROWABLE,
-    nursery::duplicate_bevy_dependencies::DUPLICATE_BEVY_DEPENDENCIES,
-    suspicious::insert_event_resource::INSERT_EVENT_RESOURCE,
-    suspicious::insert_unit_bundle::INSERT_UNIT_BUNDLE,
-    suspicious::iter_current_update_events::ITER_CURRENT_UPDATE_EVENTS,
-    pedantic::main_return_without_appexit::MAIN_RETURN_WITHOUT_APPEXIT,
-    restriction::missing_reflect::MISSING_REFLECT,
-    restriction::panicking_methods::PANICKING_METHODS,
-    style::plugin_not_ending_in_plugin::PLUGIN_NOT_ENDING_IN_PLUGIN,
-    nursery::zst_query::ZST_QUERY,
-];
-
-/// A list of all [`LintGroup`]s.
-///
-/// If a group is not in this list, it will not be registered in [`register_groups()`].
-static GROUPS: &[&LintGroup] = &[
-    // This list should be sorted alphabetically.
-    complexity::COMPLEXITY,
-    correctness::CORRECTNESS,
-    nursery::NURSERY,
-    pedantic::PEDANTIC,
-    performance::PERFORMANCE,
-    restriction::RESTRICTION,
-    style::STYLE,
-    suspicious::SUSPICIOUS,
-];
 
 /// Registers all [`BevyLint`]s in [`LINTS`] with the [`LintStore`].
 pub(crate) fn register_lints(store: &mut LintStore) {
