f3ccada31c7395fbd442ab4569a10766a84847db
DAA <42379074+DaAlbrecht@users.noreply.github.com>
2025-04-22 17:50:25 +0200

    Add lint: unconventional_naming (#345)
    Adds a new lint called `unconventional_naming` that checks whether a
    bevy trait is implemented for a structure that does not follow the
    commonly used naming pattern.
    
    So far, this only includes:
    - `SystemSet`: should end in `Set`
    - `Plugin`: should end in `Plugin`
    
    Additionally, the Lint `plugin_not_ending_in_plugin` Lint got removed as
    this is now handled in the `unconventional_naming` lint.
    
    Closes #183
    
    ---------
    
    Co-authored-by: BD103 <59022059+BD103@users.noreply.github.com>

---
+++ bevy_lint/tests/ui/unconventional_naming/system_set.rs
@@ -0,0 +1,38 @@
+#![feature(register_tool)]
+#![register_tool(bevy)]
+#![deny(bevy::unconventional_naming)]
+//~^ NOTE: the lint level is defined here
+#![allow(dead_code)]
+use bevy::prelude::*;
+
+//~v NOTE: `SystemSet` implemented here
+#[derive(SystemSet, Debug, Clone, PartialEq, Eq, Hash)]
+struct MyAudio;
+//~^ ERROR: unconventional type name for a `SystemSet`
+//~| NOTE: structures that implement `SystemSet` should end in `Set`
+//~| HELP: rename `MyAudio`
+
+// This should not raise an error since the Set ends in `Set`
+#[derive(SystemSet, Debug, Clone, PartialEq, Eq, Hash)]
+struct MyAudioSet;
+
+//~v NOTE: `SystemSet` implemented here
+#[derive(SystemSet, Debug, Clone, PartialEq, Eq, Hash)]
+struct MyAudioSystem;
+//~^ ERROR: unconventional type name for a `SystemSet`
+//~| NOTE: structures that implement `SystemSet` should end in `Set`
+//~| HELP: rename `MyAudioSystem`
+
+//~v NOTE: `SystemSet` implemented here
+#[derive(SystemSet, Debug, Clone, PartialEq, Eq, Hash)]
+struct MyAudioSystems;
+//~^ ERROR: unconventional type name for a `SystemSet`
+//~| NOTE: structures that implement `SystemSet` should end in `Set`
+//~| HELP: rename `MyAudioSystems`
+
+//~v NOTE: `SystemSet` implemented here
+#[derive(SystemSet, Debug, Clone, PartialEq, Eq, Hash)]
+struct MyAudioSteps;
+//~^ ERROR: unconventional type name for a `SystemSet`
+//~| NOTE: structures that implement `SystemSet` should end in `Set`
+//~| HELP: rename `MyAudioSteps`
