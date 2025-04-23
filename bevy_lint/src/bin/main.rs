f5d98014e8baf54b7ada821cb33f3efb022c1389
BD103 <59022059+BD103@users.noreply.github.com>
2025-04-23 14:38:32 -0400

    feat: add target library directory to path variable

--- bevy_lint/src/bin/main.rs
+++ bevy_lint/src/bin/main.rs
@@ -1,7 +1,7 @@
-use anyhow::{Context, ensure};
+use anyhow::{ensure, Context};
 use std::{
     env,
-    ffi::OsString,
+    iter,
     path::{Path, PathBuf},
     process::{Command, ExitCode, Stdio},
 };
@@ -22,28 +22,22 @@ fn main() -> anyhow::Result<ExitCode> {
     // Find the path to `bevy_lint_driver`.
     let driver_path = driver_path()?;
 
-    todo!();
+    let mut command = Command::new("cargo");
 
-    // Run `rustup run nightly-YYYY-MM-DD cargo check`.
-    let status = Command::new("rustup")
-        .arg("run")
-        .arg(RUST_TOOLCHAIN_CHANNEL)
-        .arg("cargo")
+    command
         .arg("check")
         // Forward all arguments to `cargo check` except for the first, which is the path to the
         // current executable.
         .args(std::env::args().skip(1))
         // This instructs `rustc` to call `bevy_lint_driver` instead of its default routine.
         // This lets us register custom lints.
-        .env("RUSTC_WORKSPACE_WRAPPER", driver_path)
-        // Rustup on Windows does not modify the `PATH` variable by default so a toolchain-specific
-        // version of `cargo` or `rustc` is not accidentally run instead of Rustup's proxy version.
-        // This isn't desired for us, however, because we need the `PATH` modified to discover and
-        // link to `rustc_driver.dll`. Setting `RUSTUP_WINDOWS_PATH_ADD_BIN=1` forces Rustup to
-        // modify the path. For more info, please see <https://github.com/rust-lang/rustup/pull/3703>.
-        .env("RUSTUP_WINDOWS_PATH_ADD_BIN", "1")
+        .env("RUSTC_WORKSPACE_WRAPPER", driver_path);
+
+    append_rustc_libdir(&mut command)?;
+
+    let status = command
         .status()
-        .context("Failed to spawn `cargo check`.")?;
+        .context("failed to spawn `cargo check`, is `cargo` installed?")?;
 
     let code = if status.success() {
         // Exit status of 0, success!
@@ -192,4 +186,74 @@ fn rustc_path() -> anyhow::Result<PathBuf> {
     );
 
     Ok(rustc_path.to_path_buf())
+}
+
+/// Prints the path to the Rust target library folder.
+///
+/// This folder, the "libdir", contains `librustc_driver.so`, which is needed for
+/// `bevy_lint_driver` to run. This function finds the libdir by running
+/// `rustc --print=target-libdir`.
+fn rustc_libdir() -> anyhow::Result<PathBuf> {
+    let rustc_path = rustc_path()?;
+
+    // TODO: --target parameter
+    let output = Command::new(rustc_path)
+        .arg("--print=target-libdir")
+        .stderr(Stdio::inherit())
+        .output()
+        .context("failed to spawn `rustc`")?;
+
+    ensure!(
+        output.status.success(),
+        "could not print `rustc` library path",
+    );
+
+    let libdir = Path::new(
+        // Rustup should only emit UTF-8, as it's a Rust program, so it should be safe to error
+        // here when invalid UTF-8 is found.
+        str::from_utf8(&output.stdout)
+            .context("the output of `rustc --print=target-libdir` is not valid UTF-8")?
+            .trim_end(),
+    );
+
+    ensure!(
+        libdir.is_dir(),
+        "the path returned by `rustc --print=target-libdir`, {}, does not exist",
+        libdir.display(),
+    );
+
+    debug_assert!(
+        libdir.is_absolute(),
+        "`rustc --print=target-libdir` should canonicalize the path, {}, but it is not absolute",
+        libdir.display(),
+    );
+
+    Ok(libdir.to_path_buf())
+}
+
+/// Configures a `bevy_lint_driver` [`Command`] with the correct environmental variables so that it
+/// can link to `librustc_driver.so`.
+fn append_rustc_libdir(command: &mut Command) -> anyhow::Result<()> {
+    let libdir = rustc_libdir()?;
+
+    let path_name = if cfg!(target_os = "windows") {
+        "PATH"
+    } else if cfg!(target_os = "macos") {
+        "DYLD_LIBRARY_PATH"
+    } else {
+        // We're assuming that platforms that are not Windows or MacOS are *probably* Unix-based.
+        "LD_LIBRARY_PATH"
+    };
+
+    let original_path = env::var_os(path_name)
+        .with_context(|| format!("could not fetch the `{path_name}` environmental variable"))?;
+
+    // Prepend `libdir` to the beginning of `original_path`.
+    let prepended_path =
+        env::join_paths(iter::once(libdir).chain(env::split_paths(&original_path)))
+            .context("error constructing new path environmental variable")?;
+
+    command.env(path_name, prepended_path);
+
+    Ok(())
 }
