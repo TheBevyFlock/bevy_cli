6d1990fa02eddbf5bc7d093f47c684b6694819b8
DaAlbrecht <dadidi42@gmail.com>
2025-05-02 17:24:07 +0200

    feat: install linter

--- src/lint.rs
+++ src/lint.rs
@@ -1,24 +1,34 @@
 use anyhow::{Context, anyhow, ensure};
 use std::{env, path::PathBuf};
 
+#[cfg(feature = "rustup")]
+use toml_edit::DocumentMut;
+
 use crate::external_cli::{CommandExt, cargo::install::AutoInstall};
+
+#[cfg(feature = "rustup")]
+const RUST_TOOLCHAIN: &str = include_str!("../rust-toolchain.toml");
+
+const BEVY_LINT_TAG: &str = "lint-v0.3.0";
 
 /// Runs `bevy_lint`, if it is installed, with the given arguments.
 ///
 /// Calling `lint(vec!["--workspace"])` is equivalent to calling `bevy_lint --workspace` in the
 /// terminal. This will run [`find_bevy_lint()`] to locate `bevy_lint`.
 pub fn lint(args: Vec<String>) -> anyhow::Result<()> {
-    let bevy_lint_path = find_bevy_lint()?;
+    if let Ok(bevy_lint_path) = find_bevy_lint() {
+        let status = CommandExt::new(bevy_lint_path)
+            .args(args)
+            .ensure_status(AutoInstall::Never)?;
 
-    let status = CommandExt::new(bevy_lint_path)
-        .args(args)
-        .ensure_status(AutoInstall::Never)?;
-
-    ensure!(
-        status.success(),
-        "`bevy_lint` exited with a non-zero exit code."
-    );
-
+        ensure!(
+            status.success(),
+            "`bevy_lint` exited with a non-zero exit code."
+        );
+        return Ok(());
+    }
+    #[cfg(feature = "rustup")]
+    install_linter()?;
     Ok(())
 }
 
@@ -27,7 +37,7 @@ pub fn lint(args: Vec<String>) -> anyhow::Result<()> {
 /// The current strategy will find a file named `bevy_lint(.exe)` within the same directory as the
 /// current executable, which is usually `~/.cargo/bin` or `target/debug`. It will **not** search
 /// the `PATH`.
-pub fn find_bevy_lint() -> anyhow::Result<PathBuf> {
+fn find_bevy_lint() -> anyhow::Result<PathBuf> {
     let mut bevy_lint_path = env::current_exe()
         .context("Failed to retrieve the path to the current executable.")?
         .parent()
@@ -54,4 +64,48 @@ pub fn find_bevy_lint() -> anyhow::Result<PathBuf> {
     bevy_lint_path = bevy_lint_path.canonicalize()?;
 
     Ok(bevy_lint_path)
+}
+
+fn install_linter() -> anyhow::Result<()> {
+    // TODO: pass AutoInstall args
+    let auto_install = AutoInstall::Always;
+
+    let rust_toolchain = RUST_TOOLCHAIN
+        .parse::<DocumentMut>()
+        .expect("Failed to parse `rust-toolchain.toml`.");
+
+    let channel = rust_toolchain["toolchain"]["channel"]
+        .as_str()
+        .expect("Could not find `toolchain.channel` key in `rust-toolchain.toml`.");
+
+    CommandExt::new("rustup")
+        .arg("toolchain")
+        .arg("install")
+        .arg(channel)
+        .args([
+            "--component",
+            "rustc-dev",
+            "--component",
+            "llvm-tools-preview",
+        ])
+        .ensure_status(auto_install)
+        .context(format!("failed to install toolchain `{channel}`"))?;
+
+    CommandExt::new("rustup")
+        .arg("run")
+        .arg(channel)
+        .arg("cargo")
+        .arg("install")
+        .args([
+            "--git",
+            "https://github.com/TheBevyFlock/bevy_cli.git",
+            "--tag",
+            BEVY_LINT_TAG,
+            "--locked",
+            "bevy_lint",
+        ])
+        .ensure_status(auto_install)
+        .context(format!("failed to install bevy_lint `{BEVY_LINT_TAG}`"))?;
+
+    Ok(())
 }
