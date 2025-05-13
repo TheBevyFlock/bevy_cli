ed7cb4d29aacfdcc961ba122e2cec026cd80f940
DAA <42379074+DaAlbrecht@users.noreply.github.com>
2025-05-13 09:09:13 +0200

    Support installing `bevy_lint` via CLI (#406)

--- src/lint.rs
+++ src/lint.rs
@@ -1,20 +1,68 @@
-//! Helper functions to run `bevy_lint`.
+use clap::Args;
 
-use anyhow::{Context, anyhow, ensure};
-use std::{env, path::PathBuf};
+use crate::external_cli::cargo::install::AutoInstall;
 
-use crate::external_cli::{CommandExt, cargo::install::AutoInstall};
+#[derive(Debug, Args, Clone)]
+pub struct LintArgs {
+    /// Confirm all prompts automatically.
+    #[arg(long = "yes", default_value_t = false)]
+    pub confirm_prompts: bool,
+
+    /// Arguments to forward to `cargo check`.
+    #[arg(trailing_var_arg = true, allow_hyphen_values = true)]
+    pub cargo_check_args: Vec<String>,
+}
+
+impl LintArgs {
+    /// Whether to automatically install missing dependencies.
+    // Only needed with the `rustup` feature
+    #[allow(dead_code)]
+    pub(crate) fn auto_install(&self) -> AutoInstall {
+        if self.confirm_prompts {
+            AutoInstall::Always
+        } else {
+            AutoInstall::AskUser
+        }
+    }
+}
 
 /// Runs `bevy_lint`, if it is installed, with the given arguments.
 ///
 /// Calling `lint(vec!["--workspace"])` is equivalent to calling `bevy_lint --workspace` in the
 /// terminal. This will run [`find_bevy_lint()`] to locate `bevy_lint`.
-pub fn lint(args: Vec<String>) -> anyhow::Result<()> {
-    let bevy_lint_path = find_bevy_lint()?;
+#[cfg(feature = "rustup")]
+pub fn lint(args: LintArgs) -> anyhow::Result<()> {
+    use crate::external_cli::Package;
+    use anyhow::{Context, ensure};
+    use toml_edit::DocumentMut;
 
-    let status = CommandExt::new(bevy_lint_path)
-        .args(args)
-        .ensure_status(AutoInstall::Never)?;
+    const RUST_TOOLCHAIN: &str = include_str!("../rust-toolchain.toml");
+    const BEVY_LINT_TAG: &str = "lint-v0.3.0";
+    const PACKAGE: &str = "bevy_lint";
+    const GIT_URL: &str = "https://github.com/TheBevyFlock/bevy_cli.git";
+
+    let rust_toolchain = RUST_TOOLCHAIN
+        .parse::<DocumentMut>()
+        .context("Failed to parse `rust-toolchain.toml`.")?;
+
+    let channel = rust_toolchain["toolchain"]["channel"]
+        .as_str()
+        .context("Could not find `toolchain.channel` key in `rust-toolchain.toml`.")?;
+
+    let package = Package {
+        name: PACKAGE.into(),
+        required_toolchain: Some(channel.to_string()),
+        git: Some(GIT_URL.to_string()),
+        tag: Some(BEVY_LINT_TAG.to_string()),
+        ..Default::default()
+    };
+
+    let auto_install = args.auto_install();
+
+    let status = crate::external_cli::CommandExt::new("bevy_lint")
+        .require_package(package)
+        .args(args.cargo_check_args)
+        .ensure_status(auto_install)?;
 
     ensure!(
         status.success(),
@@ -22,38 +70,4 @@ pub fn lint(args: Vec<String>) -> anyhow::Result<()> {
     );
 
     Ok(())
-}
-
-/// Tries the find the path to `bevy_lint`, if it is installed.
-///
-/// The current strategy will find a file named `bevy_lint(.exe)` within the same directory as the
-/// current executable, which is usually `~/.cargo/bin` or `target/debug`. It will **not** search
-/// the `PATH`.
-pub fn find_bevy_lint() -> anyhow::Result<PathBuf> {
-    let mut bevy_lint_path = env::current_exe()
-        .context("Failed to retrieve the path to the current executable.")?
-        .parent()
-        .ok_or(anyhow!("Path to file must have a parent."))?
-        .join("bevy_lint");
-
-    #[cfg(target_os = "windows")]
-    bevy_lint_path.set_extension("exe");
-
-    if cfg!(debug_assertions) {
-        ensure!(
-            bevy_lint_path.exists(),
-            "`bevy_lint` could not be found at {}. Please run `cargo build -p bevy_lint` first!",
-            bevy_lint_path.display(),
-        );
-    } else {
-        ensure!(
-            bevy_lint_path.exists(),
-            "`bevy_lint` could not be found at {}. Please follow the instructions at <https://thebevyflock.github.io/bevy_cli/bevy_lint/#installation> to install it.",
-            bevy_lint_path.display(),
-        );
-    }
-
-    bevy_lint_path = bevy_lint_path.canonicalize()?;
-
-    Ok(bevy_lint_path)
 }
