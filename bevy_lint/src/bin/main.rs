515daf3b940a59b217f77b15ff63b86eb2e53bbd
BD103 <59022059+BD103@users.noreply.github.com>
2025-06-11 11:36:31 -0400

    feat: support `BEVY_LINT_SYSROOT`

--- bevy_lint/src/bin/main.rs
+++ bevy_lint/src/bin/main.rs
@@ -1,6 +1,8 @@
+//! TODO: Use lowercase error messages.
+
 use anyhow::{Context, anyhow, ensure};
 use std::{
-    env,
+    env, iter,
     path::PathBuf,
     process::{Command, ExitCode},
 };
@@ -21,24 +23,78 @@ fn main() -> anyhow::Result<ExitCode> {
     // Find the path to `bevy_lint_driver`.
     let driver_path = driver_path()?;
 
-    // Run `rustup run nightly-YYYY-MM-DD cargo check`.
-    let status = Command::new("rustup")
-        .arg("run")
-        .arg(RUST_TOOLCHAIN_CHANNEL)
-        .arg("cargo")
+    // Find the path to the custom sysroot, if specified.
+    let custom_sysroot = custom_sysroot()?;
+
+    let mut cargo = match custom_sysroot {
+        // When there's a custom sysroot, run `$SYSROOT/bin/cargo`.
+        Some(sysroot) => {
+            let mut c = Command::new(sysroot.join("bin/cargo"));
+
+            let path_name = match env::consts::OS {
+                "windows" => "PATH",
+                "macos" => "DYLD_LIBRARY_PATH",
+                // Fallback to assuming the platform is Unix-based and uses `LD_LIBRARY_PATH`.
+                _ => "LD_LIBRARY_PATH",
+            };
+
+            let library_path = match env::consts::OS {
+                // `librustc_driver.dll` is in the `bin` folder on Windows.
+                "windows" => sysroot.join("bin"),
+                _ => sysroot.join("lib"),
+            };
+
+            let original_paths = env::var_os(path_name).unwrap_or_default();
+            let original_paths =
+                env::split_paths(&original_paths).filter(|path| !path.as_os_str().is_empty());
+
+            let appended_paths = original_paths.chain(iter::once(library_path));
+            let appended_paths = env::join_paths(appended_paths).with_context(|| {
+                format!("error constructing new {path_name} environmental variable")
+            })?;
+
+            // Make `librustc_driver.so` discoverable by appending its folder to the correct path
+            // environmental variable. Rustup (mostly) does this by default, but since we're using
+            // a custom sysroot we have to do it ourselves.
+            c.env(path_name, appended_paths);
+
+            c
+        }
+        // When using Rustup, run `rustup run $TOOLCHAIN cargo`.
+        None => {
+            let mut c = Command::new("rustup");
+
+            c.arg("run")
+                .arg(RUST_TOOLCHAIN_CHANNEL)
+                .arg("cargo")
+                // Between 1.27.1 and 1.28.2, Rustup by default wouldn't modify the `PATH` variable
+                // on Windows in order so a toolchain-specific version of `cargo` or `rustc` is not
+                // accidentally run instead of Rustup's proxy version. This isn't desired for us,
+                // however, because we need the `PATH` modified to discover and link to
+                // `rustc_driver.dll`. Setting `RUSTUP_WINDOWS_PATH_ADD_BIN=1` forces Rustup
+                // prepend the sysroot `bin` folder to the `PATH`.
+                //
+                // From 1.28.2 onwards, Rustup will append the `bin` folder to the `PATH` by
+                // default (which is also what we do when there's a custom sysroot). Once 1.28.2
+                // gets enough adoption (late 2025), we can remove this line and say the minimum
+                // supported Rustup version is 1.28.2.
+                //
+                // For more info, please see <https://github.com/rust-lang/rustup/pull/3703> and
+                // <https://github.com/rust-lang/rustup/pull/4249>.
+                .env("RUSTUP_WINDOWS_PATH_ADD_BIN", "1");
+
+            c
+        }
+    };
+
+    let status = cargo
         .arg("check")
         // Forward all arguments to `cargo check` except for the first, which is the path to the
         // current executable.
         .args(std::env::args().skip(1))
-        // This instructs `rustc` to call `bevy_lint_driver` instead of its default routine.
-        // This lets us register custom lints.
+        // This instructs Cargo to call `bevy_lint_driver` instead of `rustc`, which lets us use
+        // custom lints.
         .env("RUSTC_WORKSPACE_WRAPPER", driver_path)
-        // Rustup on Windows does not modify the `PATH` variable by default so a toolchain-specific
-        // version of `cargo` or `rustc` is not accidentally run instead of Rustup's proxy version.
-        // This isn't desired for us, however, because we need the `PATH` modified to discover and
-        // link to `rustc_driver.dll`. Setting `RUSTUP_WINDOWS_PATH_ADD_BIN=1` forces Rustup to
-        // modify the path. For more info, please see <https://github.com/rust-lang/rustup/pull/3703>.
-        .env("RUSTUP_WINDOWS_PATH_ADD_BIN", "1")
         .status()
         .context("Failed to spawn `cargo check`.")?;
 
@@ -93,4 +149,23 @@ fn driver_path() -> anyhow::Result<PathBuf> {
 
     // Convert the local path to the absolute path. We don't want `rustc` getting confused!
     driver_path.canonicalize().map_err(anyhow::Error::from)
+}
+
+/// Returns the path to the custom sysroot used by `bevy_lint_driver`, if specified by the user.
+///
+/// If the result is [`Some`], the path is guaranteed to exist.
+fn custom_sysroot() -> anyhow::Result<Option<PathBuf>> {
+    let sysroot = env::var_os("BEVY_LINT_SYSROOT").map(PathBuf::from);
+
+    // If the user specified `BEVY_LINT_SYSROOT`, verify it exists.
+    if let Some(ref path) = sysroot {
+        ensure!(
+            // TODO: Use `is_dir()` instead.
+            path.exists(),
+            "the path specified by `BEVY_LINT_SYSROOT`, {}, does not exist",
+            path.display(),
+        );
+    }
+
+    Ok(sysroot)
 }
