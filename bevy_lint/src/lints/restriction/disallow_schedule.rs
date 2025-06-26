fe8a86b4ffd835e698cdc5d93fd6ad0d1d6c8871
DaAlbrecht <dadidi42@gmail.com>
2025-06-26 19:00:28 +0200

    feat: ensure the receiver_ty is `App`, feat: put schedule names in backticks

--- bevy_lint/src/lints/restriction/disallow_schedule.rs
+++ bevy_lint/src/lints/restriction/disallow_schedule.rs
@@ -45,13 +45,13 @@ use crate::{declare_bevy_lint, declare_bevy_lint_pass, utils::hir_parse::MethodC
 declare_bevy_lint! {
     pub DISALLOW_FIXED_UPDATE,
     super::Restriction,
-    "defined a system in the FixedUpdate schedule",
+    "defined a system in the `FixedUpdate` schedule",
 }
 
 declare_bevy_lint! {
     pub DISALLOW_UPDATE,
     super::Restriction,
-    "defined a system in the Update schedule",
+    "defined a system in the `Update` schedule",
 }
 
 declare_bevy_lint_pass! {
@@ -72,14 +72,21 @@ impl<'tcx> LateLintPass<'tcx> for DenySchedule {
         }
 
         let Some(MethodCall {
-            method_path, args, ..
+            method_path,
+            args,
+            receiver,
+            ..
         }) = MethodCall::try_from(cx, expr)
         else {
             return;
         };
 
+        let receiver_ty = cx.typeck_results().expr_ty(receiver).peel_refs();
+
         // Match calls to `App::add_systems(schedule, systems)`
-        if method_path.ident.name != self.add_systems {
+        if !match_type(cx, receiver_ty, &crate::paths::APP)
+            || method_path.ident.name != self.add_systems
+        {
             return;
         }
 
@@ -99,11 +106,14 @@ impl<'tcx> LateLintPass<'tcx> for DenySchedule {
             schedule_type.lint(),
             schedule_label.hir_id,
             schedule_label.span,
-            format!("use of the {} schedule is disallowed", schedule_type.name()),
+            format!(
+                "use of the `{}` schedule is disallowed",
+                schedule_type.name()
+            ),
             |diag| {
                 diag.span_suggestion(
                     schedule_label.span,
-                    format!("use the {} schedule instead", schedule_type.suggestion()),
+                    format!("use the `{}` schedule instead", schedule_type.suggestion()),
                     schedule_type.suggestion(),
                     Applicability::MachineApplicable,
                 );
