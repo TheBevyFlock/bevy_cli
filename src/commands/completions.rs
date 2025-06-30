acfa0f58915ac22825474a079aa4694069453a9e
Tim Jentzsch <commits@timjen.net>
2025-06-30 18:13:28 +0200

    Improve documentation

--- src/commands/completions.rs
+++ src/commands/completions.rs
@@ -1,6 +1,7 @@
 use clap::Parser;
 use clap_complete::Shell;
 
+/// Generate code for auto-completions for the given shell.
 pub fn completions<Cli: Parser>(shell: Shell) {
     clap_complete::generate(shell, &mut Cli::command(), "bevy", &mut std::io::stdout());
 }
--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,3 +1,5 @@
+//! All available commands for the Bevy CLI.
+
 pub mod build;
 pub mod completions;
 pub mod lint;
