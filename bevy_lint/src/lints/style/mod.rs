4337ee1c108a3d989d5f5501f6ab16d7c1bca030
BD103 <59022059+BD103@users.noreply.github.com>
2025-07-14 10:44:18 -0400

    chore: remove v0.2.0 to v0.3.0 migration code

--- bevy_lint/src/lints/style/mod.rs
+++ bevy_lint/src/lints/style/mod.rs
@@ -20,16 +20,4 @@ impl LintGroup for Style {
     fn register_passes(store: &mut LintStore) {
         store.register_late_pass(|_| Box::new(unconventional_naming::UnconventionalNaming));
     }
-
-    fn register_lints(store: &mut LintStore) {
-        store.register_lints(Self::LINTS);
-
-        // `plugin_not_ending_in_plugin` was merged into `unconventional_naming` in v0.3.0. This
-        // helps users of v0.2.0 migrate to v0.3.0, but should be removed before v0.4.0 is
-        // released.
-        store.register_renamed(
-            "bevy::plugin_not_ending_in_plugin",
-            "bevy::unconventional_naming",
-        );
-    }
 }
