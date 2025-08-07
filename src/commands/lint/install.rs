c1544229e55fb05d46bddf66c636dab458c570e5
DAA <42379074+DaAlbrecht@users.noreply.github.com>
2025-08-07 16:31:25 +0200

    Support `bevy lint web` (#523)
    Support `bevy lint` and `bevy lint web` just like `build` and `run`.
    
    I added the same configuration logic to the lint command that we use in
    the other commands.
    Additionally, I removed the `rustup` feature flag from the entire`bevy
    lint` command. Instead, only the automatic installation is behind
    `rustup`
    
    This can be tested with:
    https://github.com/TimJentzsch/bevy_complex_repo/pull/7
    
    <details>
    
    <img width="610" height="804" alt="image"
    src="https://github.com/user-attachments/assets/a01900ec-8293-46cf-94ca-bf82c522fa3d"
    />
    
    </details >
    
    <img width="1161" height="318" alt="image"
    src="https://github.com/user-attachments/assets/87c9d3ca-87c6-427c-bf14-b6a0388b56e9"
    />
    
    ```sh
    ❯ bevy_lint --target wasm32-unknown-unknown
    
    error: The wasm32-unknown-unknown targets are not supported by default; you may need to enable the "wasm_js" configuration flag. Note that enabling the `wasm_js` feature flag alone is insufficient. For more information see: https://docs.rs/getrandom/0.3.3/#webassembly-support
       --> /.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/getrandom-0.3.3/src/backends.rs:168:9
        |
    168 | /         compile_error!(concat!(
    169 | |             "The wasm32-unknown-unknown targets are not supported by default; \
    170 | |             you may need to enable the \"wasm_js\" configuration flag. Note \
    171 | |             that enabling the `wasm_js` feature flag alone is insufficient. \
    172 | |             For more information see: \
    173 | |             https://docs.rs/getrandom/", env!("CARGO_PKG_VERSION"), "/#webassembly-support"
    174 | |         ));
        | |__________^
    
        Checking naga v24.0.0
    error[E0425]: cannot find function `fill_inner` in module `backends`
      --> /.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/getrandom-0.3.3/src/lib.rs:99:19
    ```
    
    <img width="973" height="272" alt="image"
    src="https://github.com/user-attachments/assets/abdbb6c7-b1e8-4a17-ae0e-216faaaa80c0"
    />
    
    <img width="1728" height="529" alt="image"
    src="https://github.com/user-attachments/assets/5131980f-e055-48c0-be22-2e58d0a99aed"
    />
    
    Closes #433
    
    ---------
    
    Co-authored-by: BD103 <59022059+BD103@users.noreply.github.com>
    
    foo
    
    foo

---
+++ src/commands/lint/install.rs
@@ -0,0 +1,170 @@
+use anyhow::{Context, ensure};
+use reqwest::blocking::Client;
+use serde::Deserialize;
+use tracing::{debug, error, info};
+
+use crate::{
+    commands::lint::InstallArgs,
+    external_cli::{cargo::install::AutoInstall, rustup},
+};
+
+#[derive(Deserialize, Debug)]
+struct RustToolchain {
+    toolchain: Toolchain,
+}
+
+#[derive(Deserialize, Debug)]
+struct Toolchain {
+    channel: String,
+}
+
+// TODO: pass auto_install after: https://github.com/TheBevyFlock/bevy_cli/pull/523
+pub(crate) fn install_linter(arg: &InstallArgs) -> anyhow::Result<()> {
+    use std::env;
+
+    const GIT_URL: &str = "https://github.com/TheBevyFlock/bevy_cli.git";
+
+    // get a list of all available `bevy_lint` versions, there should always be at least one (main).
+    let available_versions = list_available_releases()?;
+
+    let (rust_toolchain, version) = if let Some(version) = &arg.version {
+        // Check if the desired version exists, if not exit with an error message
+        if !available_versions.contains(version) {
+            error!(
+                "version: {} does not exist, available versions are: {:?}",
+                version, available_versions
+            );
+            return Ok(());
+        }
+        // return the required toolchain version, used to install via `rustup`. And the
+        // linter version used to install the linter with `cargo install --git`
+        (lookup_toolchain_version(version)?, version.clone())
+    } else {
+        // Create a `FuzzySelect` select dialog with all the available `bevy_lint` versions
+        // (including main).
+        let Some(selection) = dialoguer::FuzzySelect::new()
+            .with_prompt("Available `bevy_lint` versions")
+            .items(&available_versions)
+            .interact_opt()?
+        else {
+            debug!("no version selected");
+            return Ok(());
+        };
+
+        let version = &available_versions[selection];
+        debug!("selected {}", version);
+        // return the required toolchain version, used to install via `rustup`. And the
+        // linter version used to install the linter with `cargo install --git`
+        (lookup_toolchain_version(version)?, version.clone())
+    };
+
+    rustup::install_toolchain_if_needed(&rust_toolchain.toolchain.channel, AutoInstall::Always)?;
+
+    let mut cmd = crate::external_cli::CommandExt::new("rustup");
+
+    cmd.arg("run")
+        .arg(rust_toolchain.toolchain.channel)
+        .arg(env::var_os("BEVY_CLI_CARGO").unwrap_or("cargo".into()))
+        .arg("install")
+        .arg("--git")
+        .arg(GIT_URL);
+
+    if version == "main" {
+        cmd.arg("--branch").arg("main");
+    } else {
+        cmd.arg("--tag").arg(format!("lint-{version}"));
+    }
+
+    let status = cmd
+        .arg("--locked")
+        .arg("bevy_lint")
+        .ensure_status(AutoInstall::Always)?;
+
+    ensure!(
+        status.success(),
+        "installing `bevy_lint` exited with a non-zero exit code."
+    );
+
+    Ok(())
+}
+
+pub(crate) fn list() -> anyhow::Result<()> {
+    use std::fmt::Write;
+    let releases = list_available_releases()?;
+
+    // TODO: make this pretty
+    let mut output = String::new();
+
+    writeln!(output, "╔═════╤══════════════╗")?;
+    writeln!(output, "║  #  │ Version      ║")?;
+    writeln!(output, "╟─────┼──────────────╢")?;
+
+    for (i, version) in releases.iter().enumerate() {
+        writeln!(output, "║ {:>2}  │ {:<12} ║", i + 1, version)?;
+    }
+
+    writeln!(output, "╚═════╧══════════════╝")?;
+
+    info!("Available `bevy_lint` versions:\n{output}");
+
+    Ok(())
+}
+
+// Lists the available `bevy_lint` releases from the `GitHub` release page (including main).
+fn list_available_releases() -> anyhow::Result<Vec<String>> {
+    #[derive(Deserialize, Debug)]
+    struct Release {
+        name: String,
+    }
+
+    const URL: &str = "https://api.github.com/repos/TheBevyFlock/bevy_cli/releases";
+
+    let releases = Client::new()
+        .get(URL)
+        .header("User-Agent", "bevy_cli")
+        .send()
+        .context("failed to query available GitHub releases")?
+        .json::<Vec<Release>>()?;
+
+    let mut releases = releases
+        .iter()
+        .filter_map(|r| {
+            r.name
+                .strip_prefix("`bevy_lint` - ")
+                .map(ToString::to_string)
+        })
+        .collect::<Vec<_>>();
+
+    releases.push("main".to_string());
+
+    Ok(releases)
+}
+
+/// Looks up the `rust-toolchain.toml` file for the given version from GitHub and tries to parse it
+/// into [`RustToolchain`].
+fn lookup_toolchain_version(linter_version: &str) -> anyhow::Result<RustToolchain> {
+    let url = if linter_version == "main" {
+        "https://raw.githubusercontent.com/TheBevyFlock/bevy_cli/main/rust-toolchain.toml"
+            .to_string()
+    } else {
+        // the releases are named <`bevy_lint`-v0.3.0> but tags are only named <lint-v0.3.0>, so
+        // append `lint-`
+        format!(
+            "https://raw.githubusercontent.com/TheBevyFlock/bevy_cli/lint-{linter_version}/rust-toolchain.toml"
+        )
+    };
+
+    let response = Client::new()
+        .get(url)
+        .header("User-Agent", "bevy_cli")
+        .send()
+        .context(
+            "failed to query `rust-toolchain.toml` from GitHub for the given `bevy_lint` version",
+        )?
+        .text()?;
+
+    let rust_toolchain: RustToolchain =
+        toml::from_str(&response).context("Failed to parse `rust-toolchain.toml`.")?;
+
+    Ok(rust_toolchain)
+}
---
+++ src/commands/lint/mod.rs
@@ -0,0 +1,85 @@
+pub use args::*;
+
+use crate::{
+    bin_target::select_run_binary,
+    commands::lint::install::{install_linter, list},
+    config::CliConfig,
+    external_cli::cargo::{self, install::AutoInstall},
+};
+
+mod args;
+mod install;
+
+/// Runs `bevy_lint`, if it is installed, with the given arguments.
+///
+/// Calling `lint(vec!["--workspace"])` is equivalent to calling `bevy_lint --workspace` in the
+/// terminal.
+pub fn lint(args: &mut LintArgs) -> anyhow::Result<()> {
+    const PROGRAM: &str = "bevy_lint";
+    use anyhow::ensure;
+
+    if let Some(LintSubcommands::List) = args.subcommand {
+        return list();
+    } else if let Some(LintSubcommands::Install(args)) = &args.subcommand {
+        return install_linter(args);
+    }
+
+    let metadata = cargo::metadata::metadata()?;
+
+    let mut bin_target = select_run_binary(
+        &metadata,
+        args.cargo_args.package_args.package.as_deref(),
+        args.cargo_args.target_args.bin.as_deref(),
+        args.cargo_args.target_args.example.as_deref(),
+        args.target().as_deref(),
+        args.profile(),
+    )?;
+
+    let config = CliConfig::for_package(
+        &metadata,
+        bin_target.package,
+        args.is_web(),
+        args.is_release(),
+    )?;
+
+    args.apply_config(&config);
+    // Update the artifact directory based on the config, e.g. in case the `target` changed
+    bin_target.update_artifact_directory(
+        &metadata.target_directory,
+        args.target().as_deref(),
+        args.profile(),
+        args.cargo_args.target_args.example.is_some(),
+    );
+
+    #[cfg(feature = "web")]
+    if matches!(args.subcommand, Some(LintSubcommands::Web)) {
+        use crate::web::profiles::configure_default_web_profiles;
+
+        let mut profile_args = configure_default_web_profiles(&metadata)?;
+        // `--config` args are resolved from left to right,
+        // so the default configuration needs to come before the user args
+        profile_args.append(&mut args.cargo_args.common_args.config);
+        args.cargo_args.common_args.config = profile_args;
+    }
+
+    let cargo_args = args.cargo_args_builder();
+
+    let mut cmd = crate::external_cli::CommandExt::new("bevy_lint");
+
+    cmd.args(cargo_args)
+        .env("RUSTFLAGS", args.cargo_args.common_args.rustflags.clone());
+
+    let status = crate::external_cli::CommandExt::new("bevy_lint")
+        // We do not want to automatically install a `bevy_lint` version.
+        // The reason is that it's currently not possible to easily install
+        // the latest release without performing network requests.
+        // If we add a `required_package`, `bevy_lint` would no longer work offline.
+        .ensure_status(AutoInstall::Never)?;
+
+    ensure!(
+        status.success(),
+        "`bevy_lint` exited with a non-zero exit code."
+    );
+
+    Ok(())
+}
