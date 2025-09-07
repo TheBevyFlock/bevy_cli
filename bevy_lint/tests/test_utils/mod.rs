d1805bef82b65ef1c5b232e09eb9f2a83292cb6d
BD103 <59022059+BD103@users.noreply.github.com>
2025-09-07 00:05:02 -0400

    feat: improve error message when there is not exactly one `libbevy.rlib`

--- bevy_lint/tests/test_utils/mod.rs
+++ bevy_lint/tests/test_utils/mod.rs
@@ -7,7 +7,10 @@ use std::{
 use serde::Deserialize;
 use ui_test::{
     CommandBuilder, Config,
-    color_eyre::{self, eyre::ensure},
+    color_eyre::{
+        self,
+        eyre::{bail, ensure},
+    },
 };
 
 // This is set by Cargo to the absolute path of `bevy_lint_driver`.
@@ -120,10 +123,16 @@ fn find_bevy_rlib() -> color_eyre::Result<PathBuf> {
         }
     }
 
-    ensure!(
-        messages.len() == 1,
-        "More than one `libbevy.rlib` was built for UI tests. Please ensure there is not more than 1 version of Bevy in `Cargo.lock`.",
-    );
+    match messages.len() {
+        // Exactly 1 version of Bevy was built, so continue with the program.
+        1 => {}
+
+        // Both of these are failure cases where we exit early.
+        0 => bail!("`libbevy.rlib` was not built, but is required for ui tests"),
+        len @ 2.. => {
+            bail!("`libbevy.rlib` was built {len} times, but it was only expected to be built once")
+        }
+    };
 
     // The message usually has multiple files, often `libbevy.rlib` and `libbevy.rmeta`. Filter
     // through these to find the `rlib`.
