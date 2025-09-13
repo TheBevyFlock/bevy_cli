0a52080e33c205bfcc73207f08c7a5066e6a0fcc
DaAlbrecht <dadidi42@gmail.com>
2025-09-13 13:05:25 +0200

    feat: update insert_event_resource lint, Events -> Messages

--- bevy_lint/src/lints/suspicious/mod.rs
+++ bevy_lint/src/lints/suspicious/mod.rs
@@ -9,7 +9,7 @@ use rustc_lint::{Level, Lint, LintStore};
 
 use crate::lint::LintGroup;
 
-pub mod insert_event_resource;
+pub mod insert_message_resource;
 pub mod iter_current_update_messages;
 pub mod unit_in_bundle;
 
@@ -19,13 +19,13 @@ impl LintGroup for Suspicious {
     const NAME: &str = "bevy::suspicious";
     const LEVEL: Level = Level::Warn;
     const LINTS: &[&Lint] = &[
-        insert_event_resource::INSERT_EVENT_RESOURCE,
+        insert_message_resource::INSERT_MESSAGE_RESOURCE,
         iter_current_update_messages::ITER_CURRENT_UPDATE_MESSAGES,
         unit_in_bundle::UNIT_IN_BUNDLE,
     ];
 
     fn register_passes(store: &mut LintStore) {
-        store.register_late_pass(|_| Box::new(insert_event_resource::InsertEventResource));
+        store.register_late_pass(|_| Box::new(insert_message_resource::InsertMessageResource));
         store.register_late_pass(|_| {
             Box::new(iter_current_update_messages::IterCurrentUpdateMessages)
         });
