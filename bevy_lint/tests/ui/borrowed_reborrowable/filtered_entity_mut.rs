132b2b3a3a907fe82a2c02d863e11f51379648a8
DaAlbrecht <dadidi42@gmail.com>
2025-09-15 22:10:41 +0200

    feat: borrowed_reborrowable, fix extra lifetime for `FilteredEntityMut`

--- bevy_lint/tests/ui/borrowed_reborrowable/filtered_entity_mut.rs
+++ bevy_lint/tests/ui/borrowed_reborrowable/filtered_entity_mut.rs
@@ -1,11 +1,14 @@
-//! This tests the `borrowed_reborrowable` lint, specifically when triggered on the `FilteredEntityMut` type.
+//! This tests the `borrowed_reborrowable` lint, specifically when triggered on the
+//! `FilteredEntityMut` type.
 
 #![feature(register_tool)]
 #![register_tool(bevy)]
 #![deny(bevy::borrowed_reborrowable)]
 
-use bevy::ecs::world::{FilteredEntityMut, FilteredEntityRef};
-use bevy::prelude::*;
+use bevy::{
+    ecs::world::{FilteredEntityMut, FilteredEntityRef},
+    prelude::*,
+};
 
 // OK: Lint does not apply to immutable references
 fn immutable_reference(_param: &FilteredEntityMut) {
@@ -20,19 +23,21 @@ fn mutable_reference(_param: &mut FilteredEntityMut) {
 
 //~| HELP: use `FilteredEntityMut` instead
 //~v ERROR: parameter takes `&mut FilteredEntityMut` instead of a re-borrowed `FilteredEntityMut`
-fn mutable_reference_return<'a>(_param: &'a mut FilteredEntityMut) -> usize {
+fn mutable_reference_return(_param: &'_ mut FilteredEntityMut) -> usize {
     123
 }
 
 // OK: Lint does not apply when return type relies on reference lifetime
-fn mutable_reference_bounded_return<'a>(param: &'a mut FilteredEntityMut) -> FilteredEntityRef<'a> {
+fn mutable_reference_bounded_return<'w, 's>(
+    param: &'w mut FilteredEntityMut<'w, 's>,
+) -> FilteredEntityRef<'w, 's> {
     param.as_readonly()
 }
 
 // OK: Lint does not apply when return type relies on reference lifetime
-fn mutable_reference_bounded_return_complex<'a>(
-    param: &'a mut FilteredEntityMut,
-) -> Vec<(usize, FilteredEntityRef<'a>)> {
+fn mutable_reference_bounded_return_complex<'w, 's>(
+    param: &'w mut FilteredEntityMut<'w, 's>,
+) -> Vec<(usize, FilteredEntityRef<'w, 's>)> {
     vec![(1, param.as_readonly())]
 }
 
@@ -43,8 +48,14 @@ fn main() {
         mutable_reference(&mut param);
         _ = mutable_reference_return(&mut param);
         _ = mutable_reference_bounded_return(&mut param);
+    }
+
+    fn some_other_system(world: &mut World) {
+        let mut param: FilteredEntityMut = world.spawn_empty().into();
         _ = mutable_reference_bounded_return_complex(&mut param);
     }
 
-    App::new().add_systems(Update, some_system).run();
+    App::new()
+        .add_systems(Update, (some_system, some_other_system))
+        .run();
 }
