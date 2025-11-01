f906ce0bf5f09bd6548bd2282c84f56940755703
BD103 <59022059+BD103@users.noreply.github.com>
2025-11-01 12:03:32 -0400

    refactor: inline `base_config()` into ui test runner

--- bevy_lint/tests/ui.rs
+++ bevy_lint/tests/ui.rs
@@ -1,9 +1,44 @@
-use test_utils::base_config;
-use ui_test::run_tests;
+use std::path::{Path, PathBuf};
 
-mod test_utils;
+use ui_test::{CommandBuilder, Config, run_tests};
+
+// This is set by Cargo to the absolute path of `bevy_lint_driver`.
+const DRIVER_PATH: &str = env!("CARGO_BIN_EXE_bevy_lint_driver");
 
 fn main() {
-    let config = base_config("ui").unwrap();
+    let driver_path = Path::new(DRIVER_PATH);
+
+    assert!(
+        driver_path.is_file(),
+        "`bevy_lint_driver` could not be found at {}, make sure to build it with `cargo build -p bevy_lint --bin bevy_lint_driver`",
+        driver_path.display(),
+    );
+
+    let config = Config {
+        // When `host` is `None`, `ui_test` will attempt to auto-discover the host by calling
+        // `program -vV`. Unfortunately, `bevy_lint_driver` does not yet support the version flag,
+        // so we manually specify the host as an empty string. This means that, for now, host-
+        // specific configuration in UI tests will not work.
+        host: Some(String::new()),
+        program: CommandBuilder {
+            // We don't need `rustup run` here because we're already using the correct toolchain
+            // due to `rust-toolchain.toml`.
+            program: driver_path.into(),
+            args: vec![
+                // `bevy_lint_driver` expects the first argument to be the path to `rustc`.
+                "rustc".into(),
+                // This is required so that `ui_test` can parse warnings and errors.
+                "--error-format=json".into(),
+                todo!("include bevy dependencies"),
+            ],
+            out_dir_flag: Some("--out-dir".into()),
+            input_file_flag: None,
+            envs: Vec::new(),
+            cfg_flag: Some("--print=cfg".into()),
+        },
+        out_dir: PathBuf::from("../target/ui"),
+        ..Config::rustc(Path::new("tests/ui"))
+    };
+
     run_tests(config).unwrap();
 }
