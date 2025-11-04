4c24f51f33160a1ec3169f7d5d8670d23a39d7ee
Thomas Given <github.backslid766@passmail.net>
2025-11-03 19:26:20 -0500

    configure nightly toolchain for linter

--- flake.nix
+++ flake.nix
@@ -4,13 +4,17 @@
   inputs = {
     flake-utils.url = "github:numtide/flake-utils";
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, flake-utils, nixpkgs }:
+  outputs = { self, flake-utils, nixpkgs, rust-overlay }:
     flake-utils.lib.eachDefaultSystem (
       system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
+        pkgs = nixpkgs.legacyPackages.${system}.extend rust-overlay.overlays.default;
 
         nativeBuildInputs = with pkgs; [
           pkg-config
@@ -19,17 +23,34 @@
         buildInputs = with pkgs; [
           openssl
         ];
+
+        toolchain = pkgs.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
+
+        rustPlatform = pkgs.makeRustPlatform {
+          cargo = toolchain;
+          rustc = toolchain;
+        };
       in
       {
-        packages.default = pkgs.rustPlatform.buildRustPackage {
-          pname = "bevy";
-          version = "0.1.0-dev";
-          src = ./.;
-          cargoLock = {
-            lockFile = ./Cargo.lock;
+        packages = {
+          default = self.outputs.packages.${system}.bevy;
+          bevy = rustPlatform.buildRustPackage {
+            pname = "bevy";
+            version = "0.1.0-dev";
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+            cargoBuildFlags = [ "--all" ];
+            doCheck = false;
+
+            nativeBuildInputs = nativeBuildInputs ++ (with pkgs; [ makeBinaryWrapper ]);
+            inherit buildInputs;
+
+            postInstall = ''
+              for bin in $out/bin/bevy{,_lint}; do
+                wrapProgram $bin --set BEVY_LINT_SYSROOT ${toolchain}
+              done
+            '';
           };
-          doCheck = false;
-          inherit buildInputs nativeBuildInputs;
         };
 
         devShells.default = pkgs.mkShell {
